21029425aebe34c7b88012b5148803a8089ce2ec
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-07-08 20:56:36 +0000

    basic functions now create a multi-process mmap() scratchpad (but dont use it)

--- trunk/src/shmem/init.c
+++ trunk/src/shmem/init.c
@@ -7,27 +7,90 @@
 
 /* System libraries */
 #include <assert.h>
+#include <stddef.h>		       /* for NULL */
+#include <sys/mman.h>		       /* for mmap() and shm_open() */
+#include <sys/stat.h>		       /* for S_IRUSR and friends */
+#include <fcntl.h>		       /* for O_RDWR */
+#include <unistd.h>		       /* for unlink() and ftruncate() */
 
 /* Internals */
 #include "ptl_visibility.h"
 #include "ptl_internal_atomic.h"
 
-static unsigned int Init_Ref_Count = 0;
+static unsigned int init_ref_count = 0;
+static void *comm_pad = NULL;
+const size_t comm_pad_size = 4096 * 16;	// XXX: completely arbitrary
+const char *const comm_pad_shm_name = "portals4_comm_pad";
 
-int API_FUNC PtlInit(void)
+/* The trick to this function is making it thread-safe: multiple threads can
+ * all call PtlInit concurrently, and all will wait until initialization is
+ * complete, and if there is a failure, all will report failure. Multiple
+ * process issues (e.g. if one fails to mmap and the others succeed) are all
+ * handled transparently by the standard shm_open/shm_unlink semantics. */
+int API_FUNC PtlInit(
+    void)
 {
-    unsigned int race = PtlInternalAtomicInc(&Init_Ref_Count,1);
-    if (race == 1) {
-	/* do stuff */
-	return PTL_OK;
+    unsigned int race = PtlInternalAtomicInc(&init_ref_count, 1);
+    static int done_initializing = 0;
+    static int failure = 0;
+
+    if (race == 0) {
+	int fd;
+
+	/* Establish the communication pad */
+	assert(comm_pad == NULL);
+	fd = shm_open(comm_pad_shm_name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	assert(fd >= 0);
+	if (fd >= 0) {
+	    /* pre-allocate the shared memory... necessary on BSD */
+	    if (ftruncate(fd, comm_pad_size) != 0) {
+		goto exit_fail_mmap;
+	    }
+	    comm_pad =
+		mmap(NULL, comm_pad_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+		     fd, 0);
+	    if (comm_pad != MAP_FAILED) {
+		close(fd);
+		/* Release any concurrent initialization calls */
+		__sync_synchronize();
+		done_initializing = 1;
+		return PTL_OK;
+	    } else {
+		goto exit_fail_mmap;
+	    }
+	} else {
+	    goto exit_fail_shm;
+	}
     } else {
-	return PTL_OK;
+	/* Should block until other inits finish. */
+	while (!(volatile int)done_initializing) ;
+	if (!(volatile int)failure)
+	    return PTL_OK;
+	else
+	    goto exit_fail_fast;
     }
+  exit_fail_mmap:
+    assert(shm_unlink(comm_pad_shm_name) == 0);
+  exit_fail_shm:
+    failure = 1;
+    __sync_synchronize();
+    done_initializing = 1;
+  exit_fail_fast:
+    PtlInternalAtomicInc(&init_ref_count, -1);
+    return PTL_FAIL;
 }
 
-void API_FUNC PtlFini(void)
+void API_FUNC PtlFini(
+    void)
 {
-    assert(Init_Ref_Count > 0);
-    if (Init_Ref_Count == 0) return;
-    PtlInternalAtomicInc(&Init_Ref_Count, -1);
+    unsigned int lastone;
+    assert(init_ref_count > 0);
+    if (init_ref_count == 0)
+	return;
+    lastone = PtlInternalAtomicInc(&init_ref_count, -1);
+    if (lastone == 1) {
+	/* Clean up */
+	assert(munmap(comm_pad, comm_pad_size) == 0);
+	assert(shm_unlink(comm_pad_shm_name) == 0);
+    }
 }
---
+++ trunk/test/test_init.c
@@ -0,0 +1,12 @@
+#include <portals4.h>
+
+#include <assert.h>
+
+int main(int argc, char *argv[])
+{
+    assert(PtlInit() == PTL_OK);
+    assert(PtlInit() == PTL_OK);
+    PtlFini();
+    PtlFini();
+    return 0;
+}
