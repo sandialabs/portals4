e096d3bf2d9d708bbbd75d304867a3465f480bab
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-07-09 18:45:49 +0000

    make ICC happy

--- src/shmem/init.c
+++ src/shmem/init.c
@@ -11,7 +11,8 @@
 #include <sys/mman.h>		       /* for mmap() and shm_open() */
 #include <sys/stat.h>		       /* for S_IRUSR and friends */
 #include <fcntl.h>		       /* for O_RDWR */
-#include <unistd.h>		       /* for unlink() and ftruncate() */
+#include <stdlib.h>		       /* for getenv() */
+#include <unistd.h>		       /* for close() */
 
 /* Internals */
 #include "ptl_visibility.h"
@@ -20,15 +21,14 @@
 static unsigned int init_ref_count = 0;
 static void *comm_pad = NULL;
 const size_t comm_pad_size = 4096 * 16;	// XXX: completely arbitrary
-const char *const comm_pad_shm_name = "portals4_comm_pad";
+const char *comm_pad_shm_name = NULL;
 
 /* The trick to this function is making it thread-safe: multiple threads can
  * all call PtlInit concurrently, and all will wait until initialization is
  * complete, and if there is a failure, all will report failure. Multiple
  * process issues (e.g. if one fails to mmap and the others succeed) are all
  * handled transparently by the standard shm_open/shm_unlink semantics. */
-int API_FUNC PtlInit(
-    void)
+int API_FUNC PtlInit(void)
 {
     unsigned int race = PtlInternalAtomicInc(&init_ref_count, 1);
     static volatile int done_initializing = 0;
@@ -37,15 +37,16 @@ int API_FUNC PtlInit(
     if (race == 0) {
 	int fd;
 
-	/* Establish the communication pad */
+	/* Open the communication pad */
 	assert(comm_pad == NULL);
-	fd = shm_open(comm_pad_shm_name, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	comm_pad_shm_name = getenv("PORTALS4_SHM_NAME");
+	assert(comm_pad_shm_name != NULL);
+	if (comm_pad_shm_name) {
+	    goto exit_fail;
+	}
+	fd = shm_open(comm_pad_shm_name, O_RDWR, S_IRUSR | S_IWUSR);
 	assert(fd >= 0);
 	if (fd >= 0) {
-	    /* pre-allocate the shared memory... necessary on BSD */
-	    if (ftruncate(fd, comm_pad_size) != 0) {
-		goto exit_fail_mmap;
-	    }
 	    comm_pad =
 		mmap(NULL, comm_pad_size, PROT_READ | PROT_WRITE, MAP_SHARED,
 		     fd, 0);
@@ -56,10 +57,10 @@ int API_FUNC PtlInit(
 		done_initializing = 1;
 		return PTL_OK;
 	    } else {
-		goto exit_fail_mmap;
+		goto exit_fail;
 	    }
 	} else {
-	    goto exit_fail_shm;
+	    goto exit_fail;
 	}
     } else {
 	/* Should block until other inits finish. */
@@ -69,9 +70,7 @@ int API_FUNC PtlInit(
 	else
 	    goto exit_fail_fast;
     }
-  exit_fail_mmap:
-    assert(shm_unlink(comm_pad_shm_name) == 0);
-  exit_fail_shm:
+  exit_fail:
     failure = 1;
     __sync_synchronize();
     done_initializing = 1;
@@ -80,8 +79,7 @@ int API_FUNC PtlInit(
     return PTL_FAIL;
 }
 
-void API_FUNC PtlFini(
-    void)
+void API_FUNC PtlFini(void)
 {
     unsigned int lastone;
     assert(init_ref_count > 0);
@@ -91,6 +89,5 @@ void API_FUNC PtlFini(
     if (lastone == 1) {
 	/* Clean up */
 	assert(munmap(comm_pad, comm_pad_size) == 0);
-	assert(shm_unlink(comm_pad_shm_name) == 0);
     }
 }
