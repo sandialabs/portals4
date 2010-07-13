af6e4d5c92d215ae2ee333229fc39435cd164844
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-07-13 22:28:02 +0000

    fix up PtlGetId for handle encoding

--- src/shmem/process_identification.c
+++ src/shmem/process_identification.c
@@ -8,11 +8,13 @@
 /* System headers */
 #include <assert.h>
 #include <limits.h>		       /* for UINT_MAX */
+#include <string.h>		       /* for memcpy() */
 
 /* Internals */
 #include "ptl_internal_commpad.h"
 #include "ptl_internal_nit.h"
 #include "ptl_visibility.h"
+#include "ptl_internal_handles.h"
 
 const ptl_uid_t PTL_UID_ANY = UINT_MAX;
 
@@ -20,13 +22,17 @@ int API_FUNC PtlGetId(
     ptl_handle_ni_t ni_handle,
     ptl_process_id_t * id)
 {
+    ptl_handle_encoding_t ni;
+    memcpy(&ni, &ni_handle, sizeof(ptl_handle_ni_t));
+#ifndef NO_ARG_VALIDATION
     if (comm_pad == NULL) {
 	return PTL_NO_INIT;
     }
-    if (ni_handle > 3 || nit.refcount[ni_handle] == 0) {
+    if (ni.ni > 3 || nit.refcount[ni.ni] == 0) {
 	return PTL_ARG_INVALID;
     }
-    switch (ni_handle) {
+#endif
+    switch (ni.ni) {
 	case 0:
 	case 1:
 	    id->rank = proc_number;    // heh
