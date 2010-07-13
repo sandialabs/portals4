ba68c27598c1a1b28946e86cfcb1a128d2322306
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-07-13 19:28:17 +0000

    PtlGetId is now implemented

--- src/shmem/process_identification.c
+++ src/shmem/process_identification.c
@@ -9,10 +9,28 @@
 #include <assert.h>
 #include <limits.h>		       /* for UINT_MAX */
 
+/* Internals */
+#include "ptl_internal_commpad.h"
+#include "ptl_internal_nit.h"
+
 const ptl_uid_t PTL_UID_ANY = UINT_MAX;
 
 int PtlGetId(ptl_handle_ni_t	ni_handle,
 	     ptl_process_id_t*	id)
 {
-    return PTL_FAIL;
+    if (ni_handle > 3 || nit.refcount[ni_handle] == 0) {
+	return PTL_ARG_INVALID;
+    }
+    switch (ni_handle) {
+	case 0: case 1:
+	    id->rank = proc_number; // heh
+	    break;
+	case 2: case 3:
+	    id->phys.pid = proc_number;
+	    id->phys.nid = 0;
+	    break;
+	default:
+	    *(int *)0 = 0; // should never happen
+    }
+    return PTL_OK;
 }
