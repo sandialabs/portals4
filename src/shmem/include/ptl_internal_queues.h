d50dddf92f87dd82e44abf143dc824a28ab82d41
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-07-14 21:25:02 +0000

    atomic queues

---
+++ src/shmem/include/ptl_internal_queues.h
@@ -0,0 +1,23 @@
+#ifndef PTL_INTERNAL_QUEUES_H
+#define PTL_INTERNAL_QUEUES_H
+
+typedef struct ptl_internal_qnode_s {
+    void *value;
+    volatile struct ptl_internal_qnode_s *volatile next;
+} ptl_internal_qnode_t;
+
+typedef struct {
+    volatile ptl_internal_qnode_t *volatile head;
+    volatile ptl_internal_qnode_t *volatile tail;
+} ptl_internal_q_t;
+
+void PtlInternalQueueInit(
+    ptl_internal_q_t * q);
+void PtlInternalQueueAppend(
+    ptl_internal_q_t * q,
+    void *t);
+void *PtlInternalQueuePop(
+    ptl_internal_q_t * q);
+
+
+#endif
