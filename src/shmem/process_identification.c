b3f02dc42bc9ad4c754d1ff1968765e21fff8091
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-08-05 17:40:03 +0000

    add ptl_process_id_t validation functions

--- src/shmem/process_identification.c
+++ src/shmem/process_identification.c
@@ -12,11 +12,23 @@
 
 /* Internals */
 #include "ptl_internal_commpad.h"
+#include "ptl_internal_pid.h"
 #include "ptl_internal_nit.h"
 #include "ptl_visibility.h"
 #include "ptl_internal_handles.h"
 
 const ptl_uid_t PTL_UID_ANY = UINT_MAX;
+
+int INTERNAL PtlInternalLogicalProcessValidator(ptl_process_id_t p)
+{
+    return (p.rank >= num_siblings);
+}
+
+int INTERNAL PtlInternalPhysicalProcessValidator(ptl_process_id_t p)
+{
+    /* pid == num_siblings is the COLLECTOR */
+    return (p.phys.pid > num_siblings || p.phys.nid != 0);
+}
 
 int API_FUNC PtlGetId(
     ptl_handle_ni_t ni_handle,
@@ -33,12 +45,12 @@ int API_FUNC PtlGetId(
     }
 #endif
     switch (ni.ni) {
-	case 0:
-	case 1:
+	case 0: // Logical
+	case 1: // Logical
 	    id->rank = proc_number;    // heh
 	    break;
-	case 2:
-	case 3:
+	case 2: // Physical
+	case 3: // Physical
 	    id->phys.pid = proc_number;
 	    id->phys.nid = 0;
 	    break;
