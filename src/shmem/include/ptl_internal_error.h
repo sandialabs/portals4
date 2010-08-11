32f4bf10d10a9f36a4560d10320f5496591d888d
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-08-11 21:14:57 +0000

    include necessary headers for getenv()

--- src/shmem/include/ptl_internal_error.h
+++ src/shmem/include/ptl_internal_error.h
@@ -3,30 +3,33 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>		       /* for getenv() */
 
 static int verbose_error_flag = 0;
 
-static void VERBOSE_ERROR(const char * restrict format, ...)
+static void VERBOSE_ERROR(
+    const char *restrict format,
+    ...)
 {
     switch (verbose_error_flag) {
-        case 0:
-            if (getenv("VERBOSE") != NULL) {
-                verbose_error_flag = 2;
-                goto print_error;
-            } else {
-                verbose_error_flag = 1;
-                return;
-            }
-        case 2:
-print_error:
-            {
-                va_list ap;
-                va_start(ap, format);
-                vprintf(format, ap);
-                fflush(stdout);
-                va_end(ap);
-                break;
-            }
+	case 0:
+	    if (getenv("VERBOSE") != NULL) {
+		verbose_error_flag = 2;
+		goto print_error;
+	    } else {
+		verbose_error_flag = 1;
+		return;
+	    }
+	case 2:
+	  print_error:
+	{
+	    va_list ap;
+	    va_start(ap, format);
+	    vprintf(format, ap);
+	    fflush(stdout);
+	    va_end(ap);
+	    break;
+	}
     }
 }
 
