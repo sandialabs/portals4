72bf26247b0fc9d23c2751c330afe1b5ea97ef0b
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-08-12 20:31:22 +0000

    fix CTAlloc declaration; improve horrendous CTWait implementation

--- src/shmem/counting_events.c
+++ src/shmem/counting_events.c
@@ -28,15 +28,12 @@ const ptl_handle_ct_t PTL_CT_NONE = 0x5fffffff;	/* (2<<29) & 0x1fffffff */
 #define CT_BUSY	    1
 #define CT_READY    2
 
-typedef struct {		/* this is ordered to encourage optimal packing */
-    volatile ptl_ct_event_t data;	/* 128-bits (16 bytes) */
-    volatile uint64_t generation;	/* (8 bytes) */
-    volatile uint32_t enabled;	/* (4 bytes) 0=free, 1=busy, 2=allocated */
-    ptl_ct_type_t type;		/* enum... (4 bytes?) */
-} ptl_internal_ct_t;
+volatile uint64_t global_generation = 0;
 
-static ptl_internal_ct_t *ct_events[4] = { NULL, NULL, NULL, NULL };
-static uint64_t waiters = 0;
+static ptl_ct_event_t *ct_events[4] = { NULL, NULL, NULL, NULL };
+static uint64_t *ct_event_refcounts[4] = { NULL, NULL, NULL, NULL };
+static const ptl_ct_event_t CTERR =
+    { 0xffffffffffffffffULL, 0xffffffffffffffffULL };
 
 #define CT_NOT_EQUAL(a,b)   (a.success != b.success || a.failure != b.failure)
 #define CT_EQUAL(a,b)	    (a.success == b.success && a.failure == b.failure)
@@ -49,16 +46,16 @@ static inline int PtlInternalAtomicCasCT(
 {
 #ifdef HAVE_CMPXCHG16B
     register unsigned char ret;
-    assert(((uintptr_t)addr & 0xf) == 0);
+    assert(((uintptr_t) addr & 0xf) == 0);
     __asm__ __volatile__(
-    "lock cmpxchg16b %1\n\t"
-    "sete	     %0"
-    :"=q"(ret), "+m"(*addr)
-    :"a"     (oldval.success),
+    "lock cmpxchg16b %1\n\t" "sete	     %0":"=q"(ret),
+    "+m"    (*addr)
+    :"a"    (oldval.success),
     "d"     (oldval.failure),
     "b"     (newval.success),
     "c"     (newval.failure)
-    :"cc", "memory");
+    :"cc",
+    "memory");
     return ret;
 #else
 #error No known 128-bit atomic CAS operations are available
@@ -66,30 +63,29 @@ static inline int PtlInternalAtomicCasCT(
 }
 
 static inline void PtlInternalAtomicReadCT(
-    ptl_ct_event_t *dest, volatile ptl_ct_event_t * src)
+    ptl_ct_event_t * dest,
+    volatile ptl_ct_event_t * src)
 {
 #if defined(HAVE_READ128_INTRINSIC) && 0	/* potentially (and probably) not atomic */
     *dest = __m128i_mm_load_si128(src);
 #elif defined(HAVE_MOVDQA) && 0	       /* not actually atomic */
     __asm__ __volatile__(
-    "movdqa (%1), %%xmm0\n\t"
-    "movdqa %%xmm0, (%0)"
-    :"=r"   (dest)
+    "movdqa (%1), %%xmm0\n\t" "movdqa %%xmm0, (%0)":"=r"(dest)
     :"r"    (src)
     :"xmm0");
 #elif defined(HAVE_CMPXCHG16B)
     __asm__ __volatile__(
     "xor %%rax, %%rax\n\t"	// zeroing these out to avoid affecting *addr
-    "xor %%rbx, %%rbx\n\t"
-    "xor %%rcx, %%rcx\n\t"
-    "xor %%rdx, %%rdx\n\t"
-    "lock cmpxchg16b (%2)\n\t"
-    "mov %%rax, %0\n\t"
-    "mov %%rdx, %1\n\t"
-    :"=m"   (dest->success),
+    "xor %%rbx, %%rbx\n\t" "xor %%rcx, %%rcx\n\t" "xor %%rdx, %%rdx\n\t"
+    "lock cmpxchg16b (%2)\n\t" "mov %%rax, %0\n\t"
+    "mov %%rdx, %1\n\t":"=m"(dest->success),
     "=m"    (dest->failure)
     :"r"    (src)
-    :"cc", "rax", "rbx", "rcx", "rdx");
+    :"cc",
+    "rax",
+    "rbx",
+    "rcx",
+    "rdx");
 #else
 #error No known 128-bit atomic read operations are available
 #endif
@@ -101,15 +97,13 @@ static inline void PtlInternalAtomicWriteCT(
 {
 #ifdef HAVE_CMPXCHG16B
     __asm__ __volatile__(
-    "1:\n\t"
-    "lock cmpxchg16b %0\n\t"
-    "jne 1b"
-    :"+m" (*addr)
-    :"a"     (addr->success),
+    "1:\n\t" "lock cmpxchg16b %0\n\t" "jne 1b":"+m"(*addr)
+    :"a"    (addr->success),
     "d"     (addr->failure),
     "b"     (newval.success),
     "c"     (newval.failure)
-    :"cc", "memory");
+    :"cc",
+    "memory");
 #else
 #error No known 128-bit atomic write operations are available
 #endif
@@ -120,32 +114,35 @@ void INTERNAL PtlInternalCTNISetup(
     unsigned int ni,
     ptl_size_t limit)
 {
-    ptl_internal_ct_t *tmp;
+    ptl_ct_event_t *tmp;
     while ((tmp =
 	    PtlInternalAtomicCasPtr(&(ct_events[ni]), NULL,
 				    (void *)1)) == (void *)1) ;
     if (tmp == NULL) {
 #if defined(HAVE_MEMALIGN)
-	tmp = memalign(16, limit * sizeof(ptl_internal_ct_t));
+	tmp = memalign(16, limit * sizeof(ptl_ct_event_t));
 	assert(tmp != NULL);
-	memset(tmp, 0, limit * sizeof(ptl_internal_ct_t));
+	memset(tmp, 0, limit * sizeof(ptl_ct_event_t));
 #elif defined(HAVE_POSIX_MEMALIGN)
 	assert(posix_memalign
-	       ((void **)&tmp, 16, limit * sizeof(ptl_internal_ct_t)) == 0);
-	memset(tmp, 0, limit * sizeof(ptl_internal_ct_t));
+	       ((void **)&tmp, 16, limit * sizeof(ptl_ct_event_t)) == 0);
+	memset(tmp, 0, limit * sizeof(ptl_ct_event_t));
 #elif defined(HAVE_16ALIGNED_CALLOC)
-	tmp = calloc(limit, sizeof(ptl_internal_ct_t));
+	tmp = calloc(limit, sizeof(ptl_ct_event_t));
 	assert(tmp != NULL);
 #elif defined(HAVE_16ALIGNED_MALLOC)
-	tmp = malloc(limit * sizeof(ptl_internal_ct_t));
+	tmp = malloc(limit * sizeof(ptl_ct_event_t));
 	assert(tmp != NULL);
-	memset(tmp, 0, limit * sizeof(ptl_internal_ct_t));
+	memset(tmp, 0, limit * sizeof(ptl_ct_event_t));
 #else
-	tmp = valloc(limit * sizeof(ptl_internal_ct_t));	/* cross your fingers */
+	tmp = valloc(limit * sizeof(ptl_ct_event_t));	/* cross your fingers */
 	assert(tmp != NULL);
-	memset(tmp, 0, limit * sizeof(ptl_internal_ct_t));
+	memset(tmp, 0, limit * sizeof(ptl_ct_event_t));
 #endif
 	assert((((intptr_t) tmp) & 0x7) == 0);
+	assert(ct_event_refcounts[ni] == NULL);
+	ct_event_refcounts[ni] = calloc(limit, sizeof(uint64_t));
+	assert(ct_event_refcounts[ni] != NULL);
 	ct_events[ni] = tmp;
     }
 }
@@ -153,18 +150,24 @@ void INTERNAL PtlInternalCTNISetup(
 void INTERNAL PtlInternalCTNITeardown(
     int ni)
 {
-    ptl_internal_ct_t *tmp = ct_events[ni];
-    ct_events[ni] = NULL;
+    ptl_ct_event_t *tmp;
+    volatile uint64_t *rc;
+    while (ct_events[ni] == (void *)1) ;	// in case its in the middle of being allocated (this should never happen in sane code)
+    tmp = PtlInternalAtomicSwapPtr((void *volatile *)&ct_events[ni], NULL);
+    rc = PtlInternalAtomicSwapPtr((void *volatile *)&ct_event_refcounts[ni],
+				  NULL);
     assert(tmp != NULL);
     assert(tmp != (void *)1);
+    assert(rc != NULL);
     for (size_t i = 0; i < nit_limits.max_cts; ++i) {
-	tmp[i].data.success = 0xffffffffffffffffULL;
-	tmp[i].data.failure = 0xffffffffffffffffULL;
-	tmp[i].generation = 0xffffffffffffffffULL;
-	tmp[i].enabled = CT_FREE;
+	PtlInternalAtomicWriteCT(&(tmp[i]), CTERR);
+	PtlInternalAtomicInc(&(rc[i]), -1);
     }
-    while(waiters > 0) ;
+    for (size_t i = 0; i < nit_limits.max_cts; ++i) {
+	while (rc[i] != 0) ;
+    }
     free(tmp);
+    free((void *)rc);
 }
 
 int INTERNAL PtlInternalCTHandleValidator(
@@ -180,15 +183,18 @@ int INTERNAL PtlInternalCTHandleValidator(
     if (none_ok == 1 && handle == PTL_CT_NONE) {
 	return PTL_OK;
     }
-    if (ct.s.ni > 3 || ct.s.code > nit_limits.max_cts || (nit.refcount[ct.s.ni] == 0)) {
-	VERBOSE_ERROR("CT NI too large (%u > 3) or code is wrong (%u > %u) or nit table is uninitialized\n", ct.s.ni, ct.s.code, nit_limits.max_cts);
+    if (ct.s.ni > 3 || ct.s.code > nit_limits.max_cts ||
+	(nit.refcount[ct.s.ni] == 0)) {
+	VERBOSE_ERROR
+	    ("CT NI too large (%u > 3) or code is wrong (%u > %u) or nit table is uninitialized\n",
+	     ct.s.ni, ct.s.code, nit_limits.max_cts);
 	return PTL_ARG_INVALID;
     }
     if (ct_events[ct.s.ni] == NULL) {
 	VERBOSE_ERROR("CT table for NI uninitialized\n");
 	return PTL_ARG_INVALID;
     }
-    if (ct_events[ct.s.ni][ct.s.code].enabled != CT_READY) {
+    if (ct_event_refcounts[ct.s.ni][ct.s.code] == 0) {
 	VERBOSE_ERROR("CT appears to be deallocated\n");
 	return PTL_ARG_INVALID;
     }
@@ -198,22 +204,21 @@ int INTERNAL PtlInternalCTHandleValidator(
 
 int API_FUNC PtlCTAlloc(
     ptl_handle_ni_t ni_handle,
-    ptl_ct_type_t ct_type,
     ptl_handle_ct_t * ct_handle)
 {
-    ptl_internal_ct_t *cts;
+    ptl_ct_event_t *cts;
     ptl_size_t offset;
-    ptl_internal_handle_converter_t ni = { ni_handle };
-    ptl_handle_encoding_t ct = {.selector = HANDLE_CT_CODE,.ni = 0,.code =
-	    0 };
+    volatile uint64_t *rc;
+    const ptl_internal_handle_converter_t ni = { ni_handle };
+    ptl_handle_encoding_t ct = {.selector = HANDLE_CT_CODE,
+	.ni = 0,
+	.code = 0
+    };
 #ifndef NO_ARG_VALIDATION
     if (comm_pad == NULL) {
 	return PTL_NO_INIT;
     }
-    if (ni.s.selector != HANDLE_NI_CODE) {
-	return PTL_ARG_INVALID;
-    }
-    if (ni.s.ni > 3 || ni.s.code != 0 || (nit.refcount[ni.s.ni] == 0)) {
+    if (PtlInternalNIValidator(ni)) {
 	return PTL_ARG_INVALID;
     }
     if (ct_events[ni.s.ni] == NULL) {
@@ -225,15 +230,12 @@ int API_FUNC PtlCTAlloc(
 #endif
     ct.ni = ni.s.ni;
     cts = ct_events[ni.s.ni];
+    rc = ct_event_refcounts[ni.s.ni];
     for (offset = 0; offset < nit_limits.max_cts; ++offset) {
-	if (cts[offset].enabled == CT_FREE) {
-	    if (PtlInternalAtomicCas32
-		(&(cts[offset].enabled), CT_FREE, CT_BUSY) == CT_FREE) {
-		cts[offset].type = ct_type;
-		cts[offset].data.success = 0;
-		cts[offset].data.failure = 0;
-		__sync_synchronize();
-		cts[offset].enabled = CT_READY;
+	if (rc[offset] == 0) {
+	    if (PtlInternalAtomicCas64(&(rc[offset]), 0, 1) == 0) {
+		cts[offset].success = 0;
+		cts[offset].failure = 0;
 		ct.code = offset;
 		break;
 	    }
@@ -260,8 +262,9 @@ int API_FUNC PtlCTFree(
 	return PTL_ARG_INVALID;
     }
 #endif
-    ++ct_events[ct.s.ni][ct.s.code].generation;
-    ct_events[ct.s.ni][ct.s.code].enabled = CT_FREE;
+    PtlInternalAtomicWriteCT(&(ct_events[ct.s.ni][ct.s.code]), CTERR);
+    PtlInternalAtomicInc(&(ct_event_refcounts[ct.s.ni][ct.s.code]), -1);
+    while (ct_event_refcounts[ct.s.ni][ct.s.code] != 0) ;
     return PTL_OK;
 }
 
@@ -278,18 +281,18 @@ int API_FUNC PtlCTGet(
 	return PTL_ARG_INVALID;
     }
 #endif
-    *event = ct_events[ct.s.ni][ct.s.code].data;
+    *event = ct_events[ct.s.ni][ct.s.code];
     return PTL_OK;
 }
 
 int API_FUNC PtlCTWait(
     ptl_handle_ct_t ct_handle,
     ptl_size_t test,
-    ptl_ct_event_t *event)
+    ptl_ct_event_t * event)
 {
     const ptl_internal_handle_converter_t ct = { ct_handle };
-    ptl_internal_ct_t *cte;
-    uint64_t my_generation;
+    ptl_ct_event_t *cte;
+    volatile uint64_t *rc;
 #ifndef NO_ARG_VALIDATION
     if (comm_pad == NULL) {
 	return PTL_NO_INIT;
@@ -299,24 +302,21 @@ int API_FUNC PtlCTWait(
     }
 #endif
     cte = &(ct_events[ct.s.ni][ct.s.code]);
-    my_generation = cte->generation;
-    //printf("waiting for CT(%llu) sum to reach %llu\n", (unsigned long long)ct.i, (unsigned long long)test);
-    /* I wish this loop were tighter, but because CT's can be
-     * destroyed/reallocated unexpectedly, it can't be */
-    while (cte->generation == my_generation) {
+    rc = &(ct_event_refcounts[ct.s.ni][ct.s.code]);
+    //printf("waiting for CT(%llu) sum to reach %llu\n", (unsigned long
+    //long)ct.i, (unsigned long long)test);
+    do {
 	ptl_ct_event_t tmpread;
-	PtlInternalAtomicReadCT(&tmpread, &(cte->data));
-	if ((tmpread.success + tmpread.failure) >= test) {
-	    if (cte->generation == my_generation) {
-		assert(cte->enabled == CT_READY);
-		if (event != NULL) *event = tmpread;
-		return PTL_OK;
-	    } else {
-		return PTL_INTERRUPTED;
-	    }
+	PtlInternalAtomicReadCT(&tmpread, cte);
+	if (CT_EQUAL(tmpread, CTERR)) {
+	    PtlInternalAtomicInc(rc, -1);
+	    return PTL_INTERRUPTED;
+	} else if ((tmpread.success + tmpread.failure) >= test) {
+	    if (event != NULL)
+		*event = tmpread;
+	    return PTL_OK;
 	}
-    }
-    return PTL_INTERRUPTED;
+    } while (1);
 }
 
 int API_FUNC PtlCTSet(
@@ -332,7 +332,7 @@ int API_FUNC PtlCTSet(
 	return PTL_ARG_INVALID;
     }
 #endif
-    PtlInternalAtomicWriteCT(&(ct_events[ct.s.ni][ct.s.code].data), test);
+    PtlInternalAtomicWriteCT(&(ct_events[ct.s.ni][ct.s.code]), test);
     return PTL_OK;
 }
 
@@ -341,7 +341,7 @@ int API_FUNC PtlCTInc(
     ptl_ct_event_t increment)
 {
     const ptl_internal_handle_converter_t ct = { ct_handle };
-    ptl_internal_ct_t *cte;
+    ptl_ct_event_t *cte;
 #ifndef NO_ARG_VALIDATION
     if (comm_pad == NULL) {
 	return PTL_NO_INIT;
@@ -353,19 +353,18 @@ int API_FUNC PtlCTInc(
     cte = &(ct_events[ct.s.ni][ct.s.code]);
     if (increment.failure == 0) {
 	/* cheaper than a 128-bit atomic increment */
-	PtlInternalAtomicInc(&(cte->data.success), increment.success);
+	PtlInternalAtomicInc(&(cte->success), increment.success);
     } else if (increment.success == 0) {
 	/* cheaper than a 128-bit atomic increment */
-	PtlInternalAtomicInc(&(cte->data.success), increment.success);
+	PtlInternalAtomicInc(&(cte->failure), increment.failure);
     } else {
 	/* expensive increment */
 	ptl_ct_event_t old, tmp;
 	do {
-	    PtlInternalAtomicReadCT(&tmp, &(cte->data));
-	    old = tmp;
+	    old = tmp = *cte;
 	    tmp.success += increment.success;
 	    tmp.failure += increment.failure;
-	} while (PtlInternalAtomicCasCT(&(cte->data), old, tmp));
+	} while (PtlInternalAtomicCasCT(cte, old, tmp));
     }
     return PTL_OK;
 }
