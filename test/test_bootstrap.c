74cbe985232506044b33c125c81600ab9ffb9b6f
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-08-19 23:48:44 +0000

    more informative errors

--- test/test_bootstrap.c
+++ test/test_bootstrap.c
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define CHECK_RETURNVAL(x, call) do { switch (x) { \
+#define CHECK_RETURNVAL(x) do { switch (x) { \
 	    case PTL_OK: break; \
-	    case PTL_FAIL: fprintf(stderr, "=> %s returned PTL_FAIL (line %u)\n", call, (unsigned int)__LINE__); abort(); break; \
-	    case PTL_ARG_INVALID: fprintf(stderr, "=> %s returned PTL_ARG_INVALID (line %u)\n", call, (unsigned int)__LINE__); abort(); break; \
-	    case PTL_NO_INIT: fprintf(stderr, "=> %s returned PTL_NO_INIT (line %u)\n", call, (unsigned int)__LINE__); abort(); break; \
+	    case PTL_FAIL: fprintf(stderr, "=> " #x " returned PTL_FAIL (line %u)\n", (unsigned int)__LINE__); abort(); break; \
+	    case PTL_ARG_INVALID: fprintf(stderr, "=> " #x " returned PTL_ARG_INVALID (line %u)\n", (unsigned int)__LINE__); abort(); break; \
+	    case PTL_NO_INIT: fprintf(stderr, "=> " #x " returned PTL_NO_INIT (line %u)\n", (unsigned int)__LINE__); abort(); break; \
 	} } while (0)
 
 static void noFailures(
@@ -17,7 +17,7 @@ static void noFailures(
     ptl_size_t threshold)
 {
     ptl_ct_event_t ct_data;
-    CHECK_RETURNVAL(PtlCTWait(ct, threshold, &ct_data), "PtlCTWait");
+    CHECK_RETURNVAL(PtlCTWait(ct, threshold, &ct_data));
     assert(ct_data.failure == 0);
 }
 
@@ -36,13 +36,13 @@ int main(
     ptl_md_t md;
     ptl_handle_md_t md_handle;
 
-    CHECK_RETURNVAL(PtlInit(), "PtlInit");
+    CHECK_RETURNVAL(PtlInit());
 
     CHECK_RETURNVAL(PtlNIInit
 	   (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_PHYSICAL,
-	    PTL_PID_ANY, NULL, NULL, 0, NULL, NULL, &ni_physical), "PtlNIInit");
-    CHECK_RETURNVAL(PtlGetId(ni_physical, &myself), "PtlGetId");
-    CHECK_RETURNVAL(PtlPTAlloc(ni_physical, 0, PTL_EQ_NONE, 0, &phys_pt_index), "PtlPTAlloc");
+	    PTL_PID_ANY, NULL, NULL, 0, NULL, NULL, &ni_physical));
+    CHECK_RETURNVAL(PtlGetId(ni_physical, &myself));
+    CHECK_RETURNVAL(PtlPTAlloc(ni_physical, 0, PTL_EQ_NONE, 0, &phys_pt_index));
     assert(phys_pt_index == 0);
     /* \begin{runtime_stuff} */
     assert(getenv("PORTALS4_COLLECTOR_NID") != NULL);
@@ -95,7 +95,7 @@ int main(
 	/* cleanup */
 	assert(PtlCTFree(md.ct_handle) == PTL_OK);
 	assert(PtlMDRelease(md_handle) == PTL_OK);
-	CHECK_RETURNVAL(PtlPTFree(ni_physical, phys_pt_index), "PtlPTFree");
+	CHECK_RETURNVAL(PtlPTFree(ni_physical, phys_pt_index));
 	assert(PtlNIFini(ni_physical) == PTL_OK);
     } else {
 	/* for distributing my ID */
@@ -104,58 +104,56 @@ int main(
 	md.options = PTL_MD_EVENT_DISABLE | PTL_MD_EVENT_CT_SEND;	// count sends, but don't trigger events
 	md.eq_handle = PTL_EQ_NONE;    // i.e. don't queue send events
 	CHECK_RETURNVAL(PtlCTAlloc
-			(ni_physical, &md.ct_handle),
-			"PtlCTAlloc");
+			(ni_physical, &md.ct_handle));
 	/* for receiving the mapping */
 	le.start = dmapping;
 	le.length = (maxrank + 1) * sizeof(ptl_process_id_t);
 	le.ac_id.uid = PTL_UID_ANY;
 	le.options = PTL_LE_OP_PUT | PTL_LE_USE_ONCE | PTL_LE_EVENT_CT_PUT;
 	CHECK_RETURNVAL(PtlCTAlloc
-			(ni_physical, &le.ct_handle),
-			"PtlCTAlloc");
+			(ni_physical, &le.ct_handle));
 	/* post this now to avoid a race condition later */
 	CHECK_RETURNVAL(PtlLEAppend
 			(ni_physical, 0, le, PTL_PRIORITY_LIST, NULL,
-			 &le_handle), "PtlLEAppend");
+			 &le_handle));
 	/* now send my ID to the collector */
-	CHECK_RETURNVAL(PtlMDBind(ni_physical, &md, &md_handle), "PtlMDBind");
+	CHECK_RETURNVAL(PtlMDBind(ni_physical, &md, &md_handle));
 	CHECK_RETURNVAL(PtlPut
 			(md_handle, 0, sizeof(ptl_process_id_t),
 			 PTL_OC_ACK_REQ, COLLECTOR, 0, 0,
-			 rank * sizeof(ptl_process_id_t), NULL, 0), "PtlPut");
+			 rank * sizeof(ptl_process_id_t), NULL, 0));
 	/* wait for the send to finish */
 	noFailures(md.ct_handle, 1);
 	/* cleanup */
-	CHECK_RETURNVAL(PtlMDRelease(md_handle), "PtlMDRelease");
-	CHECK_RETURNVAL(PtlCTFree(md.ct_handle), "PtlCTFree");
+	CHECK_RETURNVAL(PtlMDRelease(md_handle));
+	CHECK_RETURNVAL(PtlCTFree(md.ct_handle));
 	/* wait to receive the mapping from the COLLECTOR */
 	noFailures(le.ct_handle, 1);
 	/* cleanup the counter */
-	CHECK_RETURNVAL(PtlCTFree(le.ct_handle), "PtlCTFree");
+	CHECK_RETURNVAL(PtlCTFree(le.ct_handle));
 	/* feed the accumulated mapping into NIInit to create the rank-based
 	 * interface */
 	CHECK_RETURNVAL(PtlNIInit
 	       (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_LOGICAL,
 		PTL_PID_ANY, NULL, NULL, maxrank+1, dmapping, amapping,
-		&ni_logical), "PtlNIInit");
-	CHECK_RETURNVAL(PtlGetId(ni_logical, &myself), "PtlGetId");
+		&ni_logical));
+	CHECK_RETURNVAL(PtlGetId(ni_logical, &myself));
 	for (int i=0; i<maxrank+1; ++i) {
 	    printf("%u's mapping[%i] = {%u,%u}\n", (unsigned int)myself.rank, i, amapping[i].phys.pid, amapping[i].phys.nid);
 	}
-	CHECK_RETURNVAL(PtlPTAlloc(ni_logical, 0, PTL_EQ_NONE, 0, &logical_pt_index), "PtlPTAlloc");
+	CHECK_RETURNVAL(PtlPTAlloc(ni_logical, 0, PTL_EQ_NONE, 0, &logical_pt_index));
 	assert(logical_pt_index == 0);
 	/* don't need this anymore, so free up resources */
-	CHECK_RETURNVAL(PtlPTFree(ni_physical, phys_pt_index), "PtlPTFree");
-	CHECK_RETURNVAL(PtlNIFini(ni_physical), "PtlNIFini");
+	CHECK_RETURNVAL(PtlPTFree(ni_physical, phys_pt_index));
+	CHECK_RETURNVAL(PtlNIFini(ni_physical));
     }
 
     /* now I can communicate between ranks with ni_logical */
     /* ... do stuff ... */
 
     /* cleanup */
-    CHECK_RETURNVAL(PtlPTFree(ni_logical, logical_pt_index), "PtlPTFree");
-    CHECK_RETURNVAL(PtlNIFini(ni_logical), "PtlNIFini");
+    CHECK_RETURNVAL(PtlPTFree(ni_logical, logical_pt_index));
+    CHECK_RETURNVAL(PtlNIFini(ni_logical));
     PtlFini();
 
     return 0;
