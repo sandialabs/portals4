a3576638efc8da28f2bf5273ac4545ee119ce96c
kbwheel@gmail.com <kbwheel@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-09-07 16:19:02 +0000

    fix race condition in exiting; properly wait for last send to finish both sending and acking

--- trunk/test/test_latency.c
+++ trunk/test/test_latency.c
@@ -49,6 +49,7 @@ int main(
     /* used in logical test */
     struct timeval start, stop;
     double accumulate = 0.0;
+    int potato = 0;
     ptl_le_t potato_catcher;
     ptl_handle_le_t potato_catcher_handle;
     ptl_md_t potato_launcher;
@@ -120,8 +121,8 @@ int main(
 		     &logical_pt_index));
     assert(logical_pt_index == 0);
     /* Now do the initial setup on ni_logical */
-    potato_catcher.start = &accumulate;
-    potato_catcher.length = sizeof(double);
+    potato_catcher.start = &potato;
+    potato_catcher.length = sizeof(potato);
     potato_catcher.ac_id.uid = PTL_UID_ANY;
     potato_catcher.options = PTL_LE_OP_PUT | PTL_LE_EVENT_CT_PUT;
     CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &potato_catcher.ct_handle));
@@ -138,9 +139,9 @@ int main(
     /* now I can communicate between ranks with ni_logical */
 
     /* set up the potato launcher */
-    potato_launcher.start = &accumulate;
+    potato_launcher.start = &potato;
     potato_launcher.length = sizeof(double);
-    potato_launcher.options = PTL_MD_EVENT_DISABLE | PTL_MD_EVENT_CT_SEND;
+    potato_launcher.options = PTL_MD_EVENT_DISABLE | PTL_MD_EVENT_CT_ACK | PTL_MD_EVENT_CT_SEND;
     potato_launcher.eq_handle = PTL_EQ_NONE;	// i.e. don't queue send events
     CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &potato_launcher.ct_handle));
     CHECK_RETURNVAL(PtlMDBind
@@ -152,8 +153,9 @@ int main(
 	nextrank.rank = myself.rank + 1;
 	nextrank.rank *= (nextrank.rank <= maxrank);
 	CHECK_RETURNVAL(PtlPut
-	       (potato_launcher_handle, 0, sizeof(double), PTL_OC_ACK_REQ,
-		nextrank, logical_pt_index, 0, 0, NULL, 1));
+			(potato_launcher_handle, 0, sizeof(double),
+			 PTL_OC_ACK_REQ, nextrank, logical_pt_index, 0, 0,
+			 NULL, 1));
 	CHECK_RETURNVAL(PtlCTWait(potato_launcher.ct_handle, 1, NULL));
 	{
 	    ptl_ct_event_t ctc = { 0, 0 };
@@ -161,44 +163,50 @@ int main(
 	}
     }
 
+    if (myself.rank == 0) {
+	assert(gettimeofday(&start, NULL) == 0);
+    }
     {				       /* the potato-passing loop */
-	size_t waitfor = 1;
+	size_t waitfor;
 	ptl_ct_event_t ctc;
 	ptl_process_t nextrank;
 	nextrank.rank = myself.rank + 1;
 	nextrank.rank *= (nextrank.rank <= maxrank);
-	while (waitfor < LOOPS) {
-	    assert(gettimeofday(&start, NULL) == 0);
+	for (waitfor = 1; waitfor <= LOOPS; ++waitfor) {
 	    CHECK_RETURNVAL(PtlCTWait(potato_catcher.ct_handle, waitfor, &ctc));	// wait for potato
-	    assert(gettimeofday(&stop, NULL) == 0);
 	    assert(ctc.failure == 0);
 	    assert(ctc.success == waitfor);
-	    ++waitfor;
-	    accumulate +=
-		(stop.tv_sec + stop.tv_usec * 1e-6) - (start.tv_sec +
-						       start.tv_usec * 1e-6);
-	    /* I have the potato! Bomb's away! */
-	    CHECK_RETURNVAL(PtlPut
-			    (potato_launcher_handle, 0,
-			     potato_launcher.length, PTL_OC_ACK_REQ, nextrank,
-			     logical_pt_index, 0, 0, NULL, 2));
+	    /* I have the potato! */
+	    ++potato;
+	    if (potato < LOOPS*(maxrank+1)) { // otherwise, the recipient may have exited
+		/* Bomb's away! */
+		CHECK_RETURNVAL(PtlPut
+			(potato_launcher_handle, 0,
+			 potato_launcher.length, PTL_OC_ACK_REQ, nextrank,
+			 logical_pt_index, 0, 0, NULL, 2));
+	    }
 	}
 	// make sure that last send completed before exiting
 	CHECK_RETURNVAL(PtlCTWait
-			(potato_launcher.ct_handle, waitfor - 1, &ctc));
+			(potato_launcher.ct_handle, (LOOPS-1)*2, &ctc));
 	assert(ctc.failure == 0);
 	if (myself.rank == 0) {
 	    // wait for the last potato (untimed)
-	    CHECK_RETURNVAL(PtlCTWait(potato_catcher.ct_handle, 1, &ctc));
+	    CHECK_RETURNVAL(PtlCTWait(potato_catcher.ct_handle, waitfor - 1, &ctc));
 	    assert(ctc.failure == 0);
 	}
     }
-
-    /* calculate the average time waiting */
     if (myself.rank == 0) {
-	printf("Total time spent waiting: %g secs\n", accumulate);
-	accumulate /= LOOPS * (maxrank + 1);
-	printf("Average time spent waiting: %g secs\n", accumulate);
+	assert(gettimeofday(&stop, NULL) == 0);
+	accumulate =
+	    (stop.tv_sec + stop.tv_usec * 1e-6) - (start.tv_sec +
+		    start.tv_usec * 1e-6);
+	/* calculate the average time waiting */
+	printf("Total time: %g secs\n", accumulate);
+	accumulate /= LOOPS;
+	printf("Average time around the loop: %g secs\n", accumulate);
+	accumulate /= maxrank + 1;
+	printf("Average latency: %g secs\n", accumulate);
     }
 
     /* cleanup */
