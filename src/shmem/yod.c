706db43854f924df785c50a68e94bbe9b1446247
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-09-16 20:04:13 +0000

    set the EVENT_DISABLE flag properly

--- src/shmem/yod.c
+++ src/shmem/yod.c
@@ -494,7 +494,7 @@ void *collator(
 	   (ni_physical, 0, le, PTL_PRIORITY_LIST, NULL, &le_handle)
 	   == PTL_OK);
     /* now distribute the mapping */
-    md.options = PTL_MD_EVENT_CT_ACK;
+    md.options = PTL_MD_EVENT_DISABLE | PTL_MD_EVENT_CT_ACK;
     md.eq_handle = PTL_EQ_NONE;
     assert(PtlCTAlloc(ni_physical, &md.ct_handle) == PTL_OK);
     assert(PtlMDBind(ni_physical, &md, &md_handle) == PTL_OK);
