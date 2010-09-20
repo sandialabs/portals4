a12bf3f5589abff052d74b8764f916390d57a3a3
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-09-20 18:37:28 +0000

    better error checking

--- trunk/test/test_LE_hotpotato.c
+++ trunk/test/test_LE_hotpotato.c
@@ -9,12 +9,15 @@
 
 #define LOOPS 1000000
 
-#define CHECK_RETURNVAL(x) do { switch (x) { \
-	    case PTL_OK: break; \
-	    case PTL_FAIL: fprintf(stderr, "=> %s returned PTL_FAIL (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
-	    case PTL_ARG_INVALID: fprintf(stderr, "=> %s returned PTL_ARG_INVALID (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
-	    case PTL_NO_INIT: fprintf(stderr, "=> %s returned PTL_NO_INIT (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
-	} } while (0)
+#define CHECK_RETURNVAL(x) do { int ret; \
+    switch (ret = x) { \
+	case PTL_OK: break; \
+	case PTL_FAIL: fprintf(stderr, "=> %s returned PTL_FAIL (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
+	case PTL_NO_SPACE: fprintf(stderr, "=> %s returned PTL_NO_SPACE (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
+	case PTL_ARG_INVALID: fprintf(stderr, "=> %s returned PTL_ARG_INVALID (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
+	case PTL_NO_INIT: fprintf(stderr, "=> %s returned PTL_NO_INIT (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
+	default: fprintf(stderr, "=> %s returned failcode %i (line %u)\n", #x, ret, (unsigned int)__LINE__); abort(); break; \
+    } } while (0)
 
 static void noFailures(
     ptl_handle_ct_t ct,
