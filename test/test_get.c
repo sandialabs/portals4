ad71668ebbcc13f347ba2f0e19b8b50c5ab36f16
kbwheel <kbwheel@gmail.com>
2010-09-27 20:23:30 +0000

    test both ME/LE get operations (with a single source file)

--- test/test_get.c
+++ test/test_get.c
@@ -32,6 +32,22 @@ static void noFailures(
     }
 }
 
+#if INTERFACE == 1
+#define ENTRY_T ptl_me_t
+#define HANDLE_T ptl_handle_me_t
+#define NI_TYPE PTL_NI_MATCHING
+#define OPTIONS (PTL_ME_OP_GET | PTL_ME_EVENT_CT_GET)
+#define APPEND PtlMEAppend
+#define UNLINK PtlMEUnlink
+#else
+#define ENTRY_T ptl_le_t
+#define HANDLE_T ptl_handle_le_t
+#define NI_TYPE PTL_NI_NO_MATCHING
+#define OPTIONS (PTL_LE_OP_GET | PTL_LE_EVENT_CT_GET)
+#define APPEND PtlLEAppend
+#define UNLINK PtlLEUnlink
+#endif
+
 int main(
     int argc,
     char *argv[])
@@ -49,8 +65,8 @@ int main(
     ptl_handle_md_t md_handle;
     /* used in logical test */
     uint64_t value, readval;
-    ptl_le_t value_le;
-    ptl_handle_le_t value_le_handle;
+    ENTRY_T value_e;
+    HANDLE_T value_e_handle;
     ptl_md_t read_md;
     ptl_handle_md_t read_md_handle;
 
@@ -111,7 +127,7 @@ int main(
     /* feed the accumulated mapping into NIInit to create the rank-based
      * interface */
     CHECK_RETURNVAL(PtlNIInit
-		    (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_LOGICAL,
+		    (PTL_IFACE_DEFAULT, NI_TYPE | PTL_NI_LOGICAL,
 		     PTL_PID_ANY, NULL, NULL, maxrank + 1, dmapping, amapping,
 		     &ni_logical));
     CHECK_RETURNVAL(PtlGetId(ni_logical, &myself));
@@ -121,14 +137,19 @@ int main(
     assert(logical_pt_index == 0);
     /* Now do the initial setup on ni_logical */
     value = myself.rank + 0xdeadbeef;
-    value_le.start = &value;
-    value_le.length = sizeof(uint64_t);
-    value_le.ac_id.uid = PTL_UID_ANY;
-    value_le.options = PTL_LE_OP_GET | PTL_LE_EVENT_CT_GET;
-    CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &value_le.ct_handle));
-    CHECK_RETURNVAL(PtlLEAppend
-		    (ni_logical, 0, value_le, PTL_PRIORITY_LIST, NULL,
-		     &value_le_handle));
+    value_e.start = &value;
+    value_e.length = sizeof(uint64_t);
+    value_e.ac_id.uid = PTL_UID_ANY;
+#if INTERFACE == 1
+    value_e.match_id.rank = PTL_RANK_ANY;
+    value_e.match_bits = 1;
+    value_e.ignore_bits = 0;
+#endif
+    value_e.options = OPTIONS;
+    CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &value_e.ct_handle));
+    CHECK_RETURNVAL(APPEND
+		    (ni_logical, 0, value_e, PTL_PRIORITY_LIST, NULL,
+		     &value_e_handle));
     /* Now do a barrier (on ni_physical) to make sure that everyone has their
      * logical interface set up */
     runtime_barrier();
@@ -154,19 +175,19 @@ int main(
 	ptl_process_t r0 = {.rank=0};
 	CHECK_RETURNVAL(PtlGet
 			(read_md_handle, 0, sizeof(uint64_t), r0,
-			 logical_pt_index, 0, NULL, 0));
+			 logical_pt_index, 1, NULL, 0));
 	CHECK_RETURNVAL(PtlCTWait(read_md.ct_handle, 1, &ctc));
 	assert(ctc.failure == 0);
     }
     printf("%i readval: %llx\n", (int)myself.rank,
 	   (unsigned long long)readval);
     if (myself.rank == 0) {
-	noFailures(value_le.ct_handle, maxrank + 1, __LINE__);
+	noFailures(value_e.ct_handle, maxrank + 1, __LINE__);
     }
     CHECK_RETURNVAL(PtlMDRelease(read_md_handle));
     CHECK_RETURNVAL(PtlCTFree(read_md.ct_handle));
-    CHECK_RETURNVAL(PtlLEUnlink(value_le_handle));
-    CHECK_RETURNVAL(PtlCTFree(value_le.ct_handle));
+    CHECK_RETURNVAL(UNLINK(value_e_handle));
+    CHECK_RETURNVAL(PtlCTFree(value_e.ct_handle));
 
     /* cleanup */
     CHECK_RETURNVAL(PtlPTFree(ni_logical, logical_pt_index));
