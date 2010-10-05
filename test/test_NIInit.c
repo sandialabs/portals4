8e1ad222a82abd857eb0d7cfb1db5a91a3d6be75
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-10-05 19:02:31 +0000

    avoid using assert

--- test/test_NIInit.c
+++ test/test_NIInit.c
@@ -10,17 +10,27 @@ int main(
     ptl_ni_limits_t actual;
     ptl_handle_ni_t ni_handle_phys;
 
-    assert(PtlNIInit
+    if (PtlNIInit
 	   (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_PHYSICAL,
 	    PTL_PID_ANY, NULL, &actual, 0, NULL, NULL,
-	    &ni_handle_phys) == PTL_NO_INIT);
-    assert(PtlInit() == PTL_OK);
-    assert(PtlInit() == PTL_OK);
-    assert(PtlNIInit
+	    &ni_handle_phys) != PTL_NO_INIT) {
+	return 1;
+    }
+    if (PtlInit() != PTL_OK) {
+	return 1;
+    }
+    if (PtlInit() != PTL_OK) {
+	return 1;
+    }
+    if (PtlNIInit
 	   (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_PHYSICAL,
 	    PTL_PID_ANY, NULL, &actual, 0, NULL, NULL,
-	    &ni_handle_phys) == PTL_OK);
-    assert(PtlNIFini(ni_handle_phys) == PTL_OK);
+	    &ni_handle_phys) != PTL_OK) {
+	return 1;
+    }
+    if (PtlNIFini(ni_handle_phys) != PTL_OK) {
+	return 1;
+    }
     PtlFini();
     PtlFini();
     return 0;
