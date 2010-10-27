a8348eee7b2d91f76550cc3428a4eaf83ef3b592
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-10-27 06:06:32 +0000

    enum to help identify functions, and #def to reduce the use of #defs elsewhere

--- src/shmem/include/ptl_internal_papi.h
+++ src/shmem/include/ptl_internal_papi.h
@@ -1,7 +1,19 @@
 #ifndef PTL_INTERNAL_PAPI_H
 #define PTL_INTERNAL_PAPI_H
 
-#include "ptl_visibility.h"
+#ifdef HAVE_LIBPAPI
+# include "ptl_visibility.h"
+
+enum {
+    PTL_PUT,
+    PTL_ME_APPEND,
+    PTL_EQ_GET,
+    PTL_EQ_PUSH,
+    PTL_LE_PROCESS,
+    PTL_ME_PROCESS,
+
+    NUM_INSTRUMENTED_PROCS // must be last entry
+} ptl_internal_papi_func;
 
 void INTERNAL PtlInternalPAPIInit(
     void);
@@ -10,11 +22,18 @@ void INTERNAL PtlInternalPAPITeardown(
 void INTERNAL PtlInternalPAPIStartC(
     void);
 void INTERNAL PtlInternalPAPISaveC(
-    int func,
+    ptl_internal_papi_func func,
     int savept);
 void INTERNAL PtlInternalPAPIDoneC(
-    int func,
+    ptl_internal_papi_func func,
     int savept);
+#else
+# define PtlInternalPAPIInit()
+# define PtlInternalPAPITeardown()
+# define PtlInternalPAPIStartC()
+# define PtlInternalPAPISaveC(x,y)
+# define PtlInternalPAPIDoneC(x,y)
+#endif /* HAVE_LIBPAPI */
 
 #endif
-/* vim:set expandtab */
+/* vim:set expandtab: */
