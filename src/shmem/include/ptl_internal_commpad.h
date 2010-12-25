e578ed3ae77a2b0fba58f74c046242534ed22f82
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2010-12-25 11:44:39 +0000

    add a new HDR_TYPE bit and mask for distinguishing truncated acks (i.e. acks that indicate the message was truncated)

--- src/shmem/include/ptl_internal_commpad.h
+++ src/shmem/include/ptl_internal_commpad.h
@@ -13,13 +13,15 @@ extern size_t proc_number;
 extern size_t per_proc_comm_buf_size;
 extern size_t firstpagesize;
 
-#define HDR_TYPE_PUT            0      /* ____ */
-#define HDR_TYPE_GET            1      /* ___1 */
-#define HDR_TYPE_ATOMIC         2      /* __1_ */
-#define HDR_TYPE_FETCHATOMIC    3      /* __11 */
-#define HDR_TYPE_SWAP           4      /* _1__ */
-#define HDR_TYPE_ACKFLAG        8      /* 1___ */
-#define HDR_TYPE_ACKMASK        7      /* _111 */
+#define HDR_TYPE_PUT            0      /* _____ */
+#define HDR_TYPE_GET            1      /* ____1 */
+#define HDR_TYPE_ATOMIC         2      /* ___1_ */
+#define HDR_TYPE_FETCHATOMIC    3      /* ___11 */
+#define HDR_TYPE_SWAP           4      /* __1__ */
+#define HDR_TYPE_ACKFLAG        8      /* _1___ */
+#define HDR_TYPE_ACKMASK        23     /* 1_111 */
+#define HDR_TYPE_TRUNCFLAG      16     /* 1____ */
+#define HDR_TYPE_BASICMASK      7      /* __111 */
 
 typedef struct {
     char *moredata;
