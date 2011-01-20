d9cf324bd3db4106a771db51bd63375876fa124b
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-01-20 23:37:06 +0000

    change test for no-arg-validation

--- trunk/test/test_NIInit.c
+++ trunk/test/test_NIInit.c
@@ -10,11 +10,13 @@ int main(
     ptl_ni_limits_t actual;
     ptl_handle_ni_t ni_handle_phys;
 
+#if 0 /* Only works if arg validation is turned on */
     if (PtlNIInit
         (PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_PHYSICAL, PTL_PID_ANY,
          NULL, &actual, 0, NULL, NULL, &ni_handle_phys) != PTL_NO_INIT) {
         return 1;
     }
+#endif
     if (PtlInit() != PTL_OK) {
         return 1;
     }
