661c037a77841cfee43340861769159768ad30b9
kbwheel@gmail.com <kbwheel@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-01-24 20:21:41 +0000

    move assertion

--- trunk/src/shmem/include/ptl_internal_locks.h
+++ trunk/src/shmem/include/ptl_internal_locks.h
@@ -10,15 +10,15 @@
 #elif defined(HAVE_PTHREAD_SPIN_INIT)
 # include <pthread.h>
 # define PTL_LOCK_TYPE pthread_spinlock_t
-# define PTL_LOCK_INIT(x) pthread_spin_init(&(x), PTHREAD_PROCESS_SHARED)
-# define PTL_LOCK_LOCK(x) pthread_spin_lock(&(x))
-# define PTL_LOCK_UNLOCK(x) pthread_spin_unlock(&(x))
+# define PTL_LOCK_INIT(x) pthread_spin_init(&(x), PTHREAD_PROCESS_PRIVATE)
+# define PTL_LOCK_LOCK(x) ptl_assert(pthread_spin_lock(&(x)), 0)
+# define PTL_LOCK_UNLOCK(x) ptl_assert(pthread_spin_unlock(&(x)), 0)
 #else
 # include <pthread.h>
 # define PTL_LOCK_TYPE pthread_mutex_t
 # define PTL_LOCK_INIT(x) pthread_mutex_init(&(x), NULL)
-# define PTL_LOCK_LOCK(x) pthread_mutex_lock(&(x))
-# define PTL_LOCK_UNLOCK(x) pthread_mutex_unlock(&(x))
+# define PTL_LOCK_LOCK(x) ptl_assert(pthread_mutex_lock(&(x)), 0)
+# define PTL_LOCK_UNLOCK(x) ptl_assert(pthread_mutex_unlock(&(x)), 0)
 #endif
 
 #endif
