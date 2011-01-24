c9c1b53528e44b0880ae088ab15c71addea0e4d6
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-01-24 19:53:05 +0000

    portable use of pthread spinlocks

---
+++ src/shmem/include/ptl_internal_locks.h
@@ -0,0 +1,24 @@
+#ifndef PTL_INTERNAL_LOCKS_H
+#define PTL_INTERNAL_LOCKS_H
+
+#ifdef __tile__
+# include <tmc/sync.h>
+# define PTL_LOCK_TYPE tmc_sync_mutex_t
+# define PTL_LOCK_INIT(x) tmc_sync_mutex_init(&(x))
+# define PTL_LOCK_LOCK(x) tmc_sync_mutex_lock(&(x))
+# define PTL_LOCK_UNLOCK(x) tmc_sync_mutex_unlock(&(x))
+#elif defined(HAVE_PTHREAD_SPIN_INIT)
+# include <pthread.h>
+# define PTL_LOCK_TYPE pthread_spinlock_t
+# define PTL_LOCK_INIT(x) pthread_spin_init(&(x), PTHREAD_PROCESS_SHARED)
+# define PTL_LOCK_LOCK(x) pthread_spin_lock(&(x))
+# define PTL_LOCK_UNLOCK(x) pthread_spin_unlock(&(x))
+#else
+# include <pthread.h>
+# define PTL_LOCK_TYPE pthread_mutex_t
+# define PTL_LOCK_INIT(x) pthread_mutex_init(&(x), NULL)
+# define PTL_LOCK_LOCK(x) pthread_mutex_lock(&(x))
+# define PTL_LOCK_UNLOCK(x) pthread_mutex_unlock(&(x))
+#endif
+
+#endif
