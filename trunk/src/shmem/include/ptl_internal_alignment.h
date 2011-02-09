a31b501b1cb81019dd2488813e33afd60b3119c5
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-02-09 18:39:13 +0000

    align PT/ME/LE allocations

--- trunk/src/shmem/include/ptl_internal_alignment.h
+++ trunk/src/shmem/include/ptl_internal_alignment.h
@@ -11,4 +11,41 @@
 # define ALIGNED(x)
 #endif
 
+#if defined(HAVE_MEMALIGN)
+# ifdef HAVE_MALLOC_H
+#  include <malloc.h>
+# endif
+# define ALIGNED_CALLOC(ret,align,count,size) do { \
+    (ret) = memalign((align), (count)*(size)); \
+    memset((ret), 0, (count)*(size)); \
+} while (0)
+# define ALIGNED_MALLOC(ret,align,size) do { \
+    (ret) = memalign((align), (size)); \
+} while (0)
+#elif defined(HAVE_POSIX_MEMALIGN)
+/* may have to define _XOPEN_SOURCE 600 */
+# include <stdlib.h>
+# define ALIGNED_CALLOC(ret,align,count,size) do { \
+    posix_memalign(&(ret), (align), (count)*(size)); \
+    memset((ret), 0, (count)*(size)); \
+} while (0)
+# define ALIGNED_MALLOC(ret,align,size) do { \
+    posix_memalign(&(ret), (align), (size)); \
+} while (0)
+#else
+# define ALIGNED_CALLOC(ret,align,count,size) do { \
+    uintptr_t tmp1 = (uintptr_t) malloc((count)*(size)+64); \
+    tmp1 &= ~(uintptr_t)(align-1); \
+    tmp1 += align; \
+    (ret) = tmp1; \
+    memset((ret), 0, (count)*(size)); \
+} while (0)
+# define ALIGNED_MALLOC(ret,align,size) do { \
+    uintptr_t tmp1 = (uintptr_t) malloc((size)+64); \
+    tmp1 &= ~(uintptr_t)(align-1); \
+    tmp1 += align; \
+    (ret) = tmp1; \
+} while (0)
+#endif
+
 #endif
