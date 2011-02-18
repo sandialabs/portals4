ae0bffbf6dddb4eafefb356580891c27fbc72931
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-02-18 18:22:06 +0000

    portal table alignment

--- src/shmem/include/ptl_internal_PT.h
+++ src/shmem/include/ptl_internal_PT.h
@@ -4,19 +4,20 @@
 #include <pthread.h>
 #include <stdint.h>                    /* for uint32_t */
 
+#include "ptl_internal_alignment.h"
 #include "ptl_internal_locks.h"
 
 typedef struct {
     PTL_LOCK_TYPE lock;
+    ptl_handle_eq_t EQ;
+    uint32_t status;
+    unsigned int options;
     struct PTqueue {
         void *head, *tail;
     } priority,
             overflow,
             buffered_headers;
-    ptl_handle_eq_t EQ;
-    uint32_t status;
-    unsigned int options;
-} ptl_table_entry_t;
+} ptl_table_entry_t ALIGNED(64);
 
 void PtlInternalPTInit(
     ptl_table_entry_t * t);
