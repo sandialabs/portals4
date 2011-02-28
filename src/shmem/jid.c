5dedc91f620ee77c452b07c794e796098666d5d1
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-02-28 19:20:30 +0000

    implement PtlGetUid and PtlGetJid (the latter is mostly just a place-hodler)

---
+++ src/shmem/jid.c
@@ -0,0 +1,39 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+/* The API definition */
+#include <portals4.h>
+
+/* System headers */
+
+/* Internals */
+#include "ptl_visibility.h"
+#ifndef NO_ARG_VALIDATION
+#include "ptl_internal_error.h"
+#include "ptl_internal_nit.h"
+#include "ptl_internal_commpad.h"
+#endif
+
+int PtlGetJid(
+    ptl_handle_ni_t ni_handle,
+    ptl_jid_t * jid)
+{
+#ifndef NO_ARG_VALIDATION
+    const ptl_internal_handle_converter_t ni = { ni_handle };
+    if (comm_pad == NULL) {
+        VERBOSE_ERROR("communication pad not initialized\n");
+        return PTL_NO_INIT;
+    }
+    if (PtlInternalNIValidator(ni)) {
+        VERBOSE_ERROR("ni code wrong\n");
+        return PTL_ARG_INVALID;
+    }
+    if (jid == NULL) {
+        VERBOSE_ERROR("jid is a null pointer\n");
+        return PTL_ARG_INVALID;
+    }
+#endif
+    *jid = PTL_JID_NONE;               // JID is somewhat ill defined... probably requires querying the job scheduler somehow
+    return PTL_OK;
+}
---
+++ src/shmem/uid.c
@@ -0,0 +1,40 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+/* The API definition */
+#include <portals4.h>
+
+/* System headers */
+#include <unistd.h>
+
+/* Internals */
+#include "ptl_visibility.h"
+#ifndef NO_ARG_VALIDATION
+#include "ptl_internal_error.h"
+#include "ptl_internal_nit.h"
+#include "ptl_internal_commpad.h"
+#endif
+
+int PtlGetUid(
+    ptl_handle_ni_t ni_handle,
+    ptl_uid_t * uid)
+{
+#ifndef NO_ARG_VALIDATION
+    const ptl_internal_handle_converter_t ni = { ni_handle };
+    if (comm_pad == NULL) {
+	VERBOSE_ERROR("communication pad not initialized\n");
+	return PTL_NO_INIT;
+    }
+    if (PtlInternalNIValidator(ni)) {
+	VERBOSE_ERROR("ni code wrong\n");
+	return PTL_ARG_INVALID;
+    }
+    if (uid == NULL) {
+	VERBOSE_ERROR("uid is a null pointer\n");
+	return PTL_ARG_INVALID;
+    }
+#endif
+    *uid = geteuid();
+    return PTL_OK;
+}
