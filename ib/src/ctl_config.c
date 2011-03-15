380b448ff485ae2c511304598508f9176e0450cf
fzsfw1 <fzsfw1@gmail.com>
2011-03-15 21:37:00 +0000

    Moved some control process global variables in the proper stryucture.

--- ib/src/ctl_config.c
+++ ib/src/ctl_config.c
@@ -1,6 +1,6 @@
 #include "ctl.h"
 
-int create_shared_memory(void)
+int create_shared_memory(struct p4oibd_config *conf)
 {
 	const int psize = getpagesize();
 	const int pmask = psize - 1;
@@ -19,40 +19,40 @@ int create_shared_memory(void)
 
 	/* Compute the sizes and offsets. */
 	size = sizeof(struct rank_table) +
-		conf.nranks * sizeof(struct rank_entry);
+		conf->nranks * sizeof(struct rank_entry);
 	size = (size + pmask) & ~pmask;
 	sc.rank_table_offset = total_size;
 	sc.rank_table_size = size;
 	total_size += size;
 
 	/* Create the file and copy the data there. */
-	sprintf(conf.shmem.filename, "/tmp/p4oibd-%d-shm-XXXXXX", getpid());
-	conf.shmem.fd = mkstemp(conf.shmem.filename);
+	sprintf(conf->shmem.filename, "/tmp/p4oibd-%d-shm-XXXXXX", getpid());
+	conf->shmem.fd = mkstemp(conf->shmem.filename);
 
-	if (conf.shmem.fd == -1)
+	if (conf->shmem.fd == -1)
 		return 1;
 
-	ret = ftruncate(conf.shmem.fd, total_size);
+	ret = ftruncate(conf->shmem.fd, total_size);
 	if (ret < 0) {
 		printf("ftruncate failed, ret = %d\n", ret);
-		close(conf.shmem.fd);
-		conf.shmem.fd = -1;
+		close(conf->shmem.fd);
+		conf->shmem.fd = -1;
 		return 1;
 	}
 
 	m = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_SHARED,
-		conf.shmem.fd, 0);
+		conf->shmem.fd, 0);
 	if (m == MAP_FAILED)
 		return 1;
 	memset(m, 0, total_size);
 
-	conf.shmem.m = (struct shared_config *)m;
-	conf.shmem.filesize = total_size;
+	conf->shmem.m = (struct shared_config *)m;
+	conf->shmem.filesize = total_size;
 
 	/* Copy the header and the tables into the shared memory. */
 	memcpy(m, &sc, sizeof(sc));
 
-	conf.master_rank_table = (struct rank_table *)(m + sc.rank_table_offset);
+	conf->master_rank_table = (struct rank_table *)(m + sc.rank_table_offset);
 
 	return 0;
 }
