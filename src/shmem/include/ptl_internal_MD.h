f91e5ee486b2265b21b9bf70c6f244b0228a3428
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-03-17 17:09:36 +0000

    compiler warnings

--- src/shmem/include/ptl_internal_MD.h
+++ src/shmem/include/ptl_internal_MD.h
@@ -1,12 +1,14 @@
 #ifndef PTL_INTERNAL_MD_H
 #define PTL_INTERNAL_MD_H
 
+#include "ptl_visibility.h"
+
 void INTERNAL       PtlInternalMDNISetup(unsigned int ni,
                                          ptl_size_t limit);
 void INTERNAL       PtlInternalMDNITeardown(unsigned int ni_num);
-char * INTERNAL     PtlInternalMDDataPtr(ptl_handle_md_t handle);
+char INTERNAL *     PtlInternalMDDataPtr(ptl_handle_md_t handle);
 ptl_size_t INTERNAL PtlInternalMDLength(ptl_handle_md_t handle);
-ptl_md_t * INTERNAL PtlInternalMDFetch(ptl_handle_md_t handle);
+ptl_md_t INTERNAL * PtlInternalMDFetch(ptl_handle_md_t handle);
 void INTERNAL       PtlInternalMDPosted(ptl_handle_md_t handle);
 void INTERNAL       PtlInternalMDCleared(ptl_handle_md_t handle);
 #ifndef NO_ARG_VALIDATION
