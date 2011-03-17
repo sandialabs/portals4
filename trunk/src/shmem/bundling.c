0970ddd9004ded31bf0a808317fd14e334814638
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-03-17 14:26:13 +0000

    uncrustified

--- trunk/src/shmem/bundling.c
+++ trunk/src/shmem/bundling.c
@@ -8,9 +8,9 @@
 /* Internals */
 #include "ptl_visibility.h"
 #ifndef NO_ARG_VALIDATION
-#include "ptl_internal_error.h"
-#include "ptl_internal_nit.h"
-#include "ptl_internal_commpad.h"
+# include "ptl_internal_error.h"
+# include "ptl_internal_nit.h"
+# include "ptl_internal_commpad.h"
 #endif
 
 int API_FUNC PtlStartBundle(ptl_handle_ni_t ni_handle)
@@ -18,12 +18,12 @@ int API_FUNC PtlStartBundle(ptl_handle_ni_t ni_handle)
 #ifndef NO_ARG_VALIDATION
     const ptl_internal_handle_converter_t ni = { ni_handle };
     if (comm_pad == NULL) {
-	VERBOSE_ERROR("communication pad not initialized\n");
-	return PTL_NO_INIT;
+        VERBOSE_ERROR("communication pad not initialized\n");
+        return PTL_NO_INIT;
     }
     if (PtlInternalNIValidator(ni)) {
-	VERBOSE_ERROR("ni code wrong\n");
-	return PTL_ARG_INVALID;
+        VERBOSE_ERROR("ni code wrong\n");
+        return PTL_ARG_INVALID;
     }
 #endif
     return PTL_OK;
@@ -34,13 +34,15 @@ int API_FUNC PtlEndBundle(ptl_handle_ni_t ni_handle)
 #ifndef NO_ARG_VALIDATION
     const ptl_internal_handle_converter_t ni = { ni_handle };
     if (comm_pad == NULL) {
-	VERBOSE_ERROR("communication pad not initialized\n");
-	return PTL_NO_INIT;
+        VERBOSE_ERROR("communication pad not initialized\n");
+        return PTL_NO_INIT;
     }
     if (PtlInternalNIValidator(ni)) {
-	VERBOSE_ERROR("ni code wrong\n");
-	return PTL_ARG_INVALID;
+        VERBOSE_ERROR("ni code wrong\n");
+        return PTL_ARG_INVALID;
     }
 #endif
     return PTL_OK;
 }
+
+/* vim:set expandtab */
