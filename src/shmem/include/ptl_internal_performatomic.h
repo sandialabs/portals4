c62c34a63355c7cc2e9292a6aee68d75a0b0eabd
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-03-30 16:46:35 +0000

    fix operand size in perform_atomic.c (and the consequences elsewhere); this fixes a gcc 4.4 ICE

--- src/shmem/include/ptl_internal_performatomic.h
+++ src/shmem/include/ptl_internal_performatomic.h
@@ -1,18 +1,18 @@
 #ifndef PTL_INTERNAL_PERFORMATOMIC_H
 #define PTL_INTERNAL_PERFORMATOMIC_H
 
-void PtlInternalPerformAtomic(char          *dest,
-                              char          *src,
-                              ptl_size_t     size,
-                              ptl_op_t       op,
-                              ptl_datatype_t dt);
+void PtlInternalPerformAtomic(uint8_t *restrict dest,
+                              uint8_t *restrict src,
+                              ptl_size_t        size,
+                              ptl_op_t          op,
+                              ptl_datatype_t    dt);
 
-void PtlInternalPerformAtomicArg(char          *dest,
-                                 char          *src,
-                                 uint64_t       operand,
-                                 ptl_size_t     size,
-                                 ptl_op_t       op,
-                                 ptl_datatype_t dt);
+void PtlInternalPerformAtomicArg(uint8_t *restrict dest,
+                                 uint8_t *restrict src,
+                                 uint8_t           operand[32],
+                                 ptl_size_t        size,
+                                 ptl_op_t          op,
+                                 ptl_datatype_t    dt);
 
 #endif /* ifndef PTL_INTERNAL_PERFORMATOMIC_H */
-/* vim:set expandtab: */
+/* vim:set expandtab ft=c: */
