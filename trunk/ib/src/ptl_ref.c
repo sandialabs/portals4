5cd5e0c6e52d83d0e0adbcb4393dd54d4af7933d
swelch@systemfabricworks.com <swelch@systemfabricworks.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-03-31 13:59:58 +0000

    leading whitespace cleanup

--- trunk/ib/src/ptl_ref.c
+++ trunk/ib/src/ptl_ref.c
@@ -6,7 +6,7 @@
 
 void ref_set(struct ref *ref, int num)
 {
-        ref->ref_cnt = num;
+	ref->ref_cnt = num;
 }
 
 void ref_init(struct ref *ref)
@@ -28,12 +28,12 @@ int ref_put(struct ref *ref, void (*release)(ref_t *ref))
 {
 	int ref_cnt;
 
-        ref_cnt = __sync_sub_and_fetch(&ref->ref_cnt, 1);
+	ref_cnt = __sync_sub_and_fetch(&ref->ref_cnt, 1);
 
 	if (ref_cnt == 0) {
-                release(ref);
-                return 1;
+	        release(ref);
+	        return 1;
 	}
 
-        return 0;
+	return 0;
 }
