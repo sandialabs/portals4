24a6ec42311529fd568d33c289df91a07e22daef
rpearson@systemfabricworks.com <rpearson@systemfabricworks.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-10-10 04:20:11 +0000

    cleanup, add comments for doxygen.

--- trunk/ib/src/ptl_md.c
+++ trunk/ib/src/ptl_md.c
@@ -1,9 +1,14 @@
-/*
- * ptl_md.c
+/**
+ * @file ptl_md.h
  */
 
 #include "ptl_loc.h"
 
+/**
+ * cleanup an md when last reference is dropped.
+ *
+ * @param[in] arg opaque address of md
+ */
 void md_cleanup(void *arg)
 {
 	md_t *md = arg;
@@ -20,14 +25,16 @@ void md_cleanup(void *arg)
 		md->ct = NULL;
 	}
 
+	for (i = 0; i < md->num_iov; i++) {
+		if (md->mr_list[i]) {
+			mr_put(md->mr_list[i]);
+			md->mr_list[i] = NULL;
+		}
+	}
+
 	if (md->sge_list_mr) {
 		mr_put(md->sge_list_mr);
 		md->sge_list_mr = NULL;
-	}
-
-	for (i=0; i<md->num_iov; i++) {
-		if (md->mr_list[i])
-			mr_put(md->mr_list[i]);
 	}
 
 	if (md->internal_data) {
@@ -35,29 +42,40 @@ void md_cleanup(void *arg)
 		md->internal_data = NULL;
 	}
 
-	pthread_spin_lock(&ni->obj.obj_lock);
-	ni->current.max_mds--;
-	pthread_spin_unlock(&ni->obj.obj_lock);
+	__sync_sub_and_fetch(&ni->current.max_mds, 1);
 }
 
-static int init_iovec(ni_t *ni, md_t *md, ptl_iovec_t *iov_list, int num_iov)
+/**
+ * Initialize iovec arrays for md.
+ *
+ * @param[in] ni the ni that md belongs to
+ * @param[in] md the md to initialize
+ * @param[in] iov_list the iovec array
+ * @param[in] num_iov the size of the iovec array
+ *
+ * @return status
+ */
+static int init_iovec(md_t *md, ptl_iovec_t *iov_list, int num_iov)
 {
 	int err;
-	int i;
+	ni_t *ni = obj_to_ni(md);
 	ptl_iovec_t *iov;
 	struct ibv_sge *sge;
 	struct shmem_iovec *knem_iovec;
 	void *p;
+	int i;
 
 	md->num_iov = num_iov;
 
-	md->internal_data = calloc(num_iov, sizeof(struct ibv_sge) + sizeof(mr_t) + sizeof(struct shmem_iovec));
+	md->internal_data = calloc(num_iov, sizeof(struct ibv_sge) +
+				   sizeof(mr_t) + sizeof(struct shmem_iovec));
 	if (!md->internal_data) {
-		WARN();
-		return PTL_NO_SPACE;
+		err = PTL_NO_SPACE;
+		goto err1;
 	}
 
 	p = md->internal_data;
+
 	md->sge_list = p;
 	p += num_iov*sizeof(struct ibv_sge);
 
@@ -71,13 +89,9 @@ static int init_iovec(ni_t *ni, md_t *md, ptl_iovec_t *iov_list, int num_iov)
 		/* Pin the whole thing. It's not big enough to make a
 		 * difference. */
 		err = mr_lookup(ni, md->internal_data,
-						p - md->internal_data,
-						&md->sge_list_mr);
-		if (err) {
-			WARN();
-			return err;
-		}
-
+				p - md->internal_data, &md->sge_list_mr);
+		if (err)
+			goto err2;
 	} else {
 		md->sge_list_mr = NULL;
 	}
@@ -93,17 +107,15 @@ static int init_iovec(ni_t *ni, md_t *md, ptl_iovec_t *iov_list, int num_iov)
 
 		md->length += iov->iov_len;
 
-		sge->addr = cpu_to_be64((uintptr_t)iov->iov_base);
-		sge->length = cpu_to_be32(iov->iov_len);
-
-		err = mr_lookup(md->obj.obj_ni, iov->iov_base, iov->iov_len, &md->mr_list[i]);
-		if (err) {
-			WARN();
-			return PTL_FAIL;
-		}
+		err = mr_lookup(ni, iov->iov_base,
+				iov->iov_len, &md->mr_list[i]);
+		if (err)
+			goto err3;
 
 		mr = md->mr_list[i];
 
+		sge->addr = cpu_to_be64((uintptr_t)iov->iov_base);
+		sge->length = cpu_to_be32(iov->iov_len);
 		sge->lkey = cpu_to_be32(mr->ibmr->rkey);
 
 		knem_iovec->cookie = mr->knem_cookie;
@@ -116,84 +128,87 @@ static int init_iovec(ni_t *ni, md_t *md, ptl_iovec_t *iov_list, int num_iov)
 	}
 
 	return PTL_OK;
+
+err3:
+	for (i--; i >= 0; i--)
+		mr_put(md->mr_list[i]);
+
+	if (md->sge_list_mr)
+		mr_put(md->sge_list_mr);
+err2:
+	free(md->internal_data);
+	md->internal_data = NULL;
+err1:
+	return err;
 }
 
+/**
+ * Create a new MD and bind to NI.
+ *
+ * @param ni_handle
+ * @param md_init
+ * @param md_handle_p
+ *
+ * @return status
+ */
 int PtlMDBind(ptl_handle_ni_t ni_handle, ptl_md_t *md_init,
-              ptl_handle_md_t *md_handle)
+              ptl_handle_md_t *md_handle_p)
 {
 	int err;
 	ni_t *ni;
 	md_t *md;
 
 	err = get_gbl();
-	if (err) {
-		WARN();
-		return err;
-	}
+	if (unlikely(err))
+		goto err0;
 
 	if (unlikely(md_init->options & ~PTL_MD_OPTIONS_MASK)) {
-		WARN();
 		err = PTL_ARG_INVALID;
 		goto err1;
 	}
 
 	err = to_ni(ni_handle, &ni);
-	if (unlikely(err)) {
-		WARN();
+	if (unlikely(err))
 		goto err1;
-	}
 
 	if (!ni) {
-		WARN();
 		err = PTL_ARG_INVALID;
 		goto err1;
 	}
 
 	err = md_alloc(ni, &md);
-	if (unlikely(err)) {
-		WARN();
+	if (unlikely(err))
 		goto err2;
-	}
 
 	if (md_init->options & PTL_IOVEC) {
 		if (md_init->length > ni->limits.max_iovecs) {
-			WARN();
 			err = PTL_ARG_INVALID;
 			goto err3;
 		}
 
-		err = init_iovec(ni, md, (ptl_iovec_t *)md_init->start,
+		err = init_iovec(md, (ptl_iovec_t *)md_init->start,
 				 md_init->length);
-
-		if (err) {
-			WARN();
+		if (err)
 			goto err3;
-		}
 	} else {
 		md->length = md_init->length;
 		md->num_iov = 0;
 	}
 
 	err = to_eq(md_init->eq_handle, &md->eq);
-	if (unlikely(err)) {
-		WARN();
+	if (unlikely(err))
 		goto err3;
-	}
 
 	if (unlikely(md->eq && (obj_to_ni(md->eq) != ni))) {
-		WARN();
 		err = PTL_ARG_INVALID;
 		goto err3;
 	}
 
 	err = to_ct(md_init->ct_handle, &md->ct);
-	if (unlikely(err)) {
-		WARN();
+	if (unlikely(err))
 		goto err3;
-	}
 
 	if (unlikely(md->ct && (obj_to_ni(md->ct) != ni))) {
-		WARN();
 		err = PTL_ARG_INVALID;
 		goto err3;
 	}
@@ -201,17 +216,15 @@ int PtlMDBind(ptl_handle_ni_t ni_handle, ptl_md_t *md_init,
 	md->start = md_init->start;
 	md->options = md_init->options;
 
-	pthread_spin_lock(&ni->obj.obj_lock);
-	ni->current.max_mds++;
-	if (unlikely(ni->current.max_mds > ni->limits.max_mds)) {
-		pthread_spin_unlock(&ni->obj.obj_lock);
-		WARN();
+	/* account for the number of MDs allocated */
+	if (unlikely(__sync_add_and_fetch(&ni->current.max_mds, 1) >
+	    ni->limits.max_mds)) {
+		__sync_sub_and_fetch(&ni->current.max_mds, 1);
 		err = PTL_NO_SPACE;
 		goto err3;
 	}
-	pthread_spin_unlock(&ni->obj.obj_lock);
 
-	*md_handle = md_to_handle(md);
+	*md_handle_p = md_to_handle(md);
 
 	ni_put(ni);
 	gbl_put();
@@ -223,47 +236,58 @@ err2:
 	ni_put(ni);
 err1:
 	gbl_put();
+err0:
 	return err;
 }
 
+/**
+ * Release MD from NI.
+ *
+ * If this is the last reference to the MD destroy the object.
+ *
+ * @param md_handle the handle of the MD to release
+ *
+ * @return status
+ */
 int PtlMDRelease(ptl_handle_md_t md_handle)
 {
 	int err;
 	md_t *md;
 
 	err = get_gbl();
-	if (unlikely(err)) {
-		WARN();
-		return err;
-	}
+	if (unlikely(err))
+		goto err0;
 
 	err = to_md(md_handle, &md);
-	if (unlikely(err)) {
-		WARN();
+	if (unlikely(err))
 		goto err1;
-	}
 
-	if (!md) {
-		WARN();
+	if (unlikely(!md)) {
 		err = PTL_ARG_INVALID;
 		goto err1;
 	}
 
-	/* There should only be 2 references on the object before we can
-	 * release it. */
+	/* simultaneous calls to PtlMDRelease, PtlGet/Put/etc, and/or
+	 * the completion of move operations can lead to races
+	 * it is the responsibility of the caller to make sure this
+	 * doesn't happen here. The dangerous case is when the caller
+	 * makes a call to a move operation and release at the same
+	 * time and the release wins and deletes the MD.  A thread safe
+	 * version would disable the MD under a lock. */
 	if (md->obj.obj_ref.ref_cnt > 2) {
-		md_put(md);	/* from to_md */
-		WARN();
 		err = PTL_IN_USE;
-		goto err1;
+		goto err2;
 	}
 
-	md_put(md);	/* from to_md */
+	md_put(md);	/* from to_md above */
 	md_put(md);	/* from alloc_md */
 	gbl_put();
 	return PTL_OK;
 
+err2:
+	md_put(md);	/* from to_md */
 err1:
 	gbl_put();
+err0:
 	return err;
 }
--- trunk/ib/src/ptl_md.h
+++ trunk/ib/src/ptl_md.h
@@ -1,29 +1,75 @@
+/**
+ * @file ptl_md.h
+ */
+
 #ifndef PTL_MD_H
 #define PTL_MD_H
 
+/* forward declarations */
 struct eq;
 struct ct;
 
-typedef struct md {
+/** md object info */
+struct md {
+	/** object base class */
 	obj_t			obj;
+
+	/** start of buff or iovec array from md_init */
 	void			*start;
+
+	/** number of entries in iovec arrays */
 	unsigned int		num_iov;
+
+	/** size of md data from md_init length or sum of iovecs */
 	ptl_size_t		length;
+
+	/** md options */
 	unsigned int		options;
+
+	/** optional event queue or NULL */
 	struct eq		*eq;
+
+	/** optional counting event or NULL */
 	struct ct		*ct;
 
-	mr_t *sge_list_mr;			/* MR for sge_list */
+	/** allocated memory used to hold iovec arrays
+	 * or NULL if num_iov is zero */
+	void			*internal_data;
 
-	void *internal_data;
-	mr_t **mr_list;				/* MRs for the SGEs */
-	struct ibv_sge		*sge_list; /* list of SGE */
-	struct shmem_iovec *knem_iovecs; /* list for KNEM */
+	/** scatter gather list passed to the target that
+	 * contains a list of addresses, lengths and rkeys
+	 * in network byte order for use by messages sent
+	 * over the OFA verbs API */
+	struct ibv_sge		*sge_list;
 
-} md_t;
+	/** mrs to register memory regions for verbs API
+	 * can hold one mr per iovec, these are not allocated
+	 * until the md or part of an mr is used to send/recv
+	 * a message, contained in internal_data  */
+	mr_t			**mr_list;
+
+	/** list of knem info for each iovec for use in long
+	 * messages send through shared memory */
+	struct shmem_iovec	*knem_iovecs;
+
+	/** mr to register long array of iovecs passed to target
+	 * when num_iov exceeds the amount that will fit in a
+	 * short message */
+	mr_t			*sge_list_mr;
+};
+
+typedef struct md md_t;
 
 void md_cleanup(void *arg);
 
+/**
+ * Allocate a new md object.
+ *
+ * @param ni the ni for which to allocate an md
+ * @param md_p the location in which to return the md
+ *
+ * @return status
+ */
 static inline int md_alloc(ni_t *ni, md_t **md_p)
 {
 	int err;
@@ -39,6 +85,14 @@ static inline int md_alloc(ni_t *ni, md_t **md_p)
 	return PTL_OK;
 }
 
+/**
+ * Convert an md handle to an md object.
+ *
+ * @param handle the md handle
+ * @param md_p the location in which to return the md
+ *
+ * @return status
+ */
 static inline int to_md(ptl_handle_md_t handle, md_t **md_p)
 {
 	int err;
@@ -54,16 +108,35 @@ static inline int to_md(ptl_handle_md_t handle, md_t **md_p)
 	return PTL_OK;
 }
 
+/**
+ * Take a reference on an md.
+ *
+ * @param md the md on which to take a reference
+ */
 static inline void md_get(md_t *md)
 {
 	obj_get(&md->obj);
 }
 
+/**
+ * Drop a reference to an md.
+ *
+ * @param md the md from which to drop the deference
+ *
+ * @return status
+ */
 static inline int md_put(md_t *md)
 {
 	return obj_put(&md->obj);
 }
 
+/**
+ * Get md handle from md object.
+ *
+ * @param md the md from which to get handle
+ *
+ * @return the handle of the md
+ */
 static inline ptl_handle_md_t md_to_handle(md_t *md)
 {
         return (ptl_handle_md_t)md->obj.obj_handle;
