5289e6eeed77f88fff85e56673174c11fd5cd135
rpearson@systemfabricworks.com <rpearson@systemfabricworks.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-10-13 05:09:17 +0000

    added comments for doxygen formatting added mutex and cond

--- trunk/ib/src/ptl_eq.h
+++ trunk/ib/src/ptl_eq.h
@@ -1,25 +1,77 @@
+/**
+ * @file ptl_eq.h
+ *
+ * Event queue interface declarations.
+ * @see ptl_eq.c
+ */
+
 #ifndef PTL_EQ_H
 #define PTL_EQ_H
 
-typedef struct eqe {
-	unsigned int		generation;
-	ptl_event_t		event;
-} eqe_t;
+/**
+ * Event queue entry.
+ */
+struct eqe {
+	unsigned int		generation;	/**< increments each time
+						     the producer pointer
+						     wraps */
+	ptl_event_t		event;		/**< portals event */
+};
 
-typedef struct eq {
-	obj_t			obj;
-	eqe_t			*eqe_list;
-	unsigned int		count;
-	unsigned int		producer;
-	unsigned int		consumer;
-	unsigned int		prod_gen;
-	unsigned int		cons_gen;
-	int			interrupt;
-	int			overflow;
-} eq_t;
+typedef struct eqe eqe_t;
+
+/**
+ * Event queue info.
+ */
+struct eq {
+	obj_t			obj;		/**< object base class */
+	eqe_t			*eqe_list;	/**< circular buffer for
+						     holding events */
+	unsigned int		count;		/**< size of event queue */
+	unsigned int		producer;	/**< producer index */
+	unsigned int		consumer;	/**< consumer index */
+	unsigned int		prod_gen;	/**< producer generation */
+	unsigned int		cons_gen;	/**< consumer generation */
+	int			interrupt;	/**< if set eq is being
+						     freed or destroyed */
+	int			overflow;	/**< true if producer has
+						     wrapped the consumer */
+	pthread_mutex_t		mutex;		/**< mutex for eq condition */
+	pthread_cond_t		cond;		/**< condition to break out
+						     of eq wait calls */
+	unsigned int		waiters;	/**< number of waiters for
+						     eq condition */
+
+};
+
+typedef struct eq eq_t;
+
+int eq_init(void *arg, void *unused);
+
+void eq_fini(void *arg);
+
+int eq_new(void *arg);
 
 void eq_cleanup(void *arg);
 
+void make_init_event(xi_t *xi, eq_t *eq, ptl_event_kind_t type, void *start);
+
+void make_target_event(xt_t *xt, eq_t *eq, ptl_event_kind_t type,
+		       void *user_ptr, void *start);
+
+void make_le_event(le_t *le, eq_t *eq, ptl_event_kind_t type,
+		   ptl_ni_fail_t fail_type);
+
+/**
+ * Allocate a new eq object.
+ *
+ * Takes a reference on the eq object.
+ *
+ * @param[in] ni the ni for which to allocate the eq
+ * @param eq_p[out] address of the returned eq
+ *
+ * @return status
+ */
 static inline int eq_alloc(ni_t *ni, eq_t **eq_p)
 {
 	int err;
@@ -35,6 +87,16 @@ static inline int eq_alloc(ni_t *ni, eq_t **eq_p)
 	return PTL_OK;
 }
 
+/**
+ * Convert from an eq handle to the eq object.
+ *
+ * Takes a reference to the object.
+ *
+ * @param[in] eq_handle the handle of the eq object
+ * @param eq_p[out] address of the returned eq
+ *
+ * @return status
+ */
 static inline int to_eq(ptl_handle_eq_t eq_handle, eq_t **eq_p)
 {
 	int err;
@@ -50,25 +112,40 @@ static inline int to_eq(ptl_handle_eq_t eq_handle, eq_t **eq_p)
 	return PTL_OK;
 }
 
+/**
+ * Take a reference on an eq object.
+ *
+ * @param[in] eq the eq object
+ */
 static inline void eq_get(eq_t *eq)
 {
 	obj_get(&eq->obj);
 }
 
+/**
+ * Drop a reference to an eq object.
+ *
+ * When the last reference is dropped the eq is freed.
+ *
+ * @param[in] eq the eq object
+ *
+ * @return status
+ */
 static inline int eq_put(eq_t *eq)
 {
 	return obj_put(&eq->obj);
 }
 
+/**
+ * Convert eq object to its handle.
+ *
+ * @param[in] eq the eq object
+ *
+ * @return eq handle
+ */
 static inline ptl_handle_eq_t eq_to_handle(eq_t *eq)
 {
-        return (ptl_handle_eq_t)eq->obj.obj_handle;
+	return (ptl_handle_eq_t)eq->obj.obj_handle;
 }
-
-void make_init_event(xi_t *xi, eq_t *eq, ptl_event_kind_t type, void *start);
-
-void make_target_event(xt_t *xt, eq_t *eq, ptl_event_kind_t type, void *user_ptr, void *start);
-
-void make_le_event(le_t *le, eq_t *eq, ptl_event_kind_t type, ptl_ni_fail_t fail_type);
 
 #endif /* PTL_EQ_H */
