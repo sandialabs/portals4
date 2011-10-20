31d3d9fa8fa2ec5949ebfb0624db4842b0a4a1cf
bwbarrett <bwbarrett@gmail.com>
2011-10-20 03:25:39 +0000

    Start of adding support for IB implementation to test code.  Can either use 'single' or 'mpi' runtimes.  Single is single process, mpi uses mpi for launch and coordination.  FOr now, only build the sfw test suite if mpi.

---
+++ test/support/single.c
@@ -0,0 +1,86 @@
+/* -*- C -*-
+ *
+ * Copyright 2011 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S.  Government
+ * retains certain rights in this software.
+ * 
+ * This file is part of the Portals SHMEM software package. For license
+ * information, see the LICENSE file in the top level directory of the
+ * distribution.
+ *
+ *
+ * Run-time support for the built-in runtime that is part of the
+ * shared memory implementation Portals
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <portals4.h>
+
+#include "support.h"
+
+static ptl_process_t my_id;
+static ptl_handle_ni_t phys_ni_h;
+
+int
+libtest_init(void)
+{
+    int ret;
+
+    ret = PtlNIInit(PTL_IFACE_DEFAULT,
+                    PTL_NI_NO_MATCHING | PTL_NI_PHYSICAL,
+                    PTL_PID_ANY,
+                    NULL,
+                    NULL,
+                    &phys_ni_h);
+    if (PTL_OK != ret) return 1;
+
+    ret = PtlGetId(phys_ni_h, &my_id);
+    if (PTL_OK != ret) return 1;
+
+    return 0;
+}
+
+
+int
+libtest_fini(void)
+{
+    PtlNIFini(phys_ni_h);
+
+    return 0;
+}
+
+
+ptl_process_t*
+libtest_get_mapping(void)
+{
+    ptl_process_t *ret;
+
+    ret = malloc(sizeof(ptl_process_t) * 1);
+    if (NULL == ret) return NULL;
+
+    ret[0] = my_id;
+    return ret;
+}
+
+
+int
+libtest_get_rank(void)
+{
+    return 0;
+}
+
+
+int
+libtest_get_size(void)
+{
+    return 1;
+}
+
+
+void
+libtest_barrier(void)
+{
+}
