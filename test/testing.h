06af8783cffdf09f25c806f624255006331e2123
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-10-24 15:12:10 +0000

    PTL_IGNORED is similar to PTL_OK

--- test/testing.h
+++ test/testing.h
@@ -5,6 +5,7 @@
 
 #define CHECK_RETURNVAL(x) do { int ret; \
     switch (ret = x) { \
+	case PTL_IGNORED: \
         case PTL_OK: break; \
         case PTL_FAIL: fprintf(stderr, "=> %s returned PTL_FAIL (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
         case PTL_NO_SPACE: fprintf(stderr, "=> %s returned PTL_NO_SPACE (line %u)\n", #x, (unsigned int)__LINE__); abort(); break; \
