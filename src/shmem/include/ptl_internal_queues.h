051b426119b197e8f818e3f8a7d143aafbfa91d3
kbwheel <kbwheel@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-11-01 16:36:41 +0000

    remove volatile (doesnt do anything useful)

--- src/shmem/include/ptl_internal_queues.h
+++ src/shmem/include/ptl_internal_queues.h
@@ -2,13 +2,13 @@
 #define PTL_INTERNAL_QUEUES_H
 
 typedef struct ptl_internal_qnode_s {
-    void                                          *value;
-    volatile struct ptl_internal_qnode_s *volatile next;
+    void                        *value;
+    struct ptl_internal_qnode_s *next;
 } ptl_internal_qnode_t;
 
 typedef struct {
-    volatile ptl_internal_qnode_t *volatile head;
-    volatile ptl_internal_qnode_t *volatile tail;
+    ptl_internal_qnode_t *head;
+    ptl_internal_qnode_t *tail;
 } ptl_internal_q_t;
 
 void PtlInternalQueueInit(ptl_internal_q_t *q);
