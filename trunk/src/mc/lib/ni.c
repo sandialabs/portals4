1255e69d5ebb35534ed354c8273fd3444c6262e6
mjleven@sandia.gov <mjleven@sandia.gov@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-11-02 19:15:04 +0000

    add some command queue code

--- trunk/src/mc/lib/ni.c
+++ trunk/src/mc/lib/ni.c
@@ -1,31 +1,33 @@
 #include "portals4.h"
 #include "ppe_if.h"
 
-int PtlNIInit(ptl_interface_t   iface,
-              unsigned int      options,
-              ptl_pid_t         pid,
-              const ptl_ni_limits_t   *desired,
-              ptl_ni_limits_t   *actual,
-              ptl_handle_ni_t   *ni_handle)
+ptl_cq_t* _cq;
+
+int PtlNIInit(ptl_interface_t       iface,
+              unsigned int          options,
+              ptl_pid_t             pid,
+              const ptl_ni_limits_t *desired,
+              ptl_ni_limits_t       *actual,
+              ptl_handle_ni_t       *ni_handle)
 {
-    return FUNC_CALL( PtlNIInit, iface, options, pid, 
+    return FUNC_CALL( _cq, PtlNIInit, iface, options, pid, 
                                     desired, actual, ni_handle );
 }
 
 int PtlNIFini(ptl_handle_ni_t ni_handle)
 {
-    return FUNC_CALL( PtlNIFini, ni_handle );
+    return FUNC_CALL( _cq, PtlNIFini, ni_handle );
 }
 
 int PtlNIHandle(ptl_handle_any_t    handle,
                 ptl_handle_ni_t*    ni_handle)
 {
-    return FUNC_CALL( PtlNIHandle, handle, ni_handle ); 
+    return FUNC_CALL( _cq, PtlNIHandle, handle, ni_handle ); 
 }
 
 int PtlNIStatus(ptl_handle_ni_t    handle,
                 ptl_sr_index_t     status_register,
                 ptl_sr_value_t     *status )
 {
-    return FUNC_CALL( PtlNIStatus, handle, status_register, status );
+    return FUNC_CALL( _cq, PtlNIStatus, handle, status_register, status );
 }
--- trunk/src/mc/lib/ppe_if_ni.h
+++ trunk/src/mc/lib/ppe_if_ni.h
@@ -2,32 +2,54 @@
 #ifndef PPE_IF_NI_H
 #define PPE_IF_NI_H
 
-static inline int if_PtlNIInit(ptl_interface_t   iface,
-              unsigned int      options,
-              ptl_pid_t         pid,
-              const ptl_ni_limits_t   *desired,
-              ptl_ni_limits_t   *actual,
-              ptl_handle_ni_t   *ni_handle)
+#include "command_queue.h"
+
+static inline int 
+if_PtlNIInit( ptl_cq_t* cq, 
+                ptl_interface_t   iface,
+                unsigned int      options,
+                ptl_pid_t         pid,
+                const ptl_ni_limits_t   *desired,
+                ptl_ni_limits_t   *actual,
+                ptl_handle_ni_t   *ni_handle)
 {
-    return PTL_FAIL;
+    ptl_cqe_t *entry;
+    ptl_cq_entry_alloc( cq, &entry );
+    ptl_cq_entry_send( cq, entry );
+    return PTL_OK;
 }
 
-static inline int if_PtlNIFini(ptl_handle_ni_t ni_handle)
+static inline int 
+if_PtlNIFini( ptl_cq_t* cq,
+                ptl_handle_ni_t ni_handle)
 {
-    return PTL_FAIL;
+    ptl_cqe_t *entry;
+    ptl_cq_entry_alloc( cq, &entry );
+    ptl_cq_entry_send( cq, entry );
+    return PTL_OK;
 }
 
-static inline int if_PtlNIHandle(ptl_handle_any_t    handle,
+static inline int 
+if_PtlNIHandle( ptl_cq_t* cq, 
+                ptl_handle_any_t    handle,
                 ptl_handle_ni_t*    ni_handle)
 {
-    return PTL_FAIL;
+    ptl_cqe_t *entry;
+    ptl_cq_entry_alloc( cq, &entry );
+    ptl_cq_entry_send( cq, entry );
+    return PTL_OK;
 }
 
-static inline int if_PtlNIStatus(ptl_handle_ni_t    handle,
+static inline int 
+if_PtlNIStatus( ptl_cq_t* cq,
+                ptl_handle_ni_t    handle,
                 ptl_sr_index_t     status_register,
                 ptl_sr_value_t     *status )
 {
-    return PTL_FAIL;
+    ptl_cqe_t *entry;
+    ptl_cq_entry_alloc( cq, &entry );
+    ptl_cq_entry_send( cq, entry );
+    return PTL_OK;
 }
 
 #endif
