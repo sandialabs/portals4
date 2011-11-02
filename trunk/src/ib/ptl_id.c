d9fb360f25f3d4814d7faa8abcfd376a249bece8
rpearson@systemfabricworks.com <rpearson@systemfabricworks.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-11-02 02:51:52 +0000

    added more inline documentation added #ifdef's for parameter checking

--- trunk/src/ib/ptl_id.c
+++ trunk/src/ib/ptl_id.c
@@ -1,84 +1,95 @@
-/*
- * ptl_id.c
+/**
+ * @file ptl_id.c
+ *
+ * @brief Portals ID APIs.
  */
 
 #include "ptl_loc.h"
 
-/* can return
-PTL OK
-PTL NI INVALID
-PTL NO INIT
-PTL ARG_INVALID
-*/
-int PtlGetUid(ptl_handle_ni_t ni_handle, ptl_uid_t *uid)
+/**
+ * @brief Get the user identifier of a process.
+ *
+ * @param[in] ni_handle The handle of the NI.
+ * @param[out] uid_p The address of the returned user id.
+ *
+ * @return PTL_OK Indicates success.
+ * @return PTL_ARG_INVALID Indicates that an invalid argument was passed.
+ * @return PTL_NO_INIT Indicates that the portals API has not been successfully initialized.
+ */
+int PtlGetUid(ptl_handle_ni_t ni_handle, ptl_uid_t *uid_p)
 {
 	int err;
 	ni_t *ni;
 
+#ifndef NO_ARG_VALIDATION
 	err = gbl_get();
 	if (err)
-		return err;
+		goto err0;
 
 	err = to_ni(ni_handle, &ni);
-	if (unlikely(err))
+	if (err)
 		goto err1;
 
 	if (!ni) {
 		err = PTL_ARG_INVALID;
 		goto err1;
 	}
+#else
+	ni = fast_to_obj(ni_handle);
+#endif
 
-	*uid = ni->uid;
+	*uid_p = ni->uid;
 
+	err = PTL_OK;
 	ni_put(ni);
-	gbl_put();
-	return PTL_OK;
-
+#ifndef NO_ARG_VALIDATION
 err1:
 	gbl_put();
+err0:
+#endif
 	return err;
 }
 
-/* can return
-PTL OK
-PTL NI INVALID
-PTL NO INIT
-PTL ARG_INVALID
-*/
-int PtlGetId(ptl_handle_ni_t ni_handle, ptl_process_t *id)
+/**
+ * @brief Get the process identifier of a process.
+ *
+ * @param[in] ni_handle The handle of the NI.
+ * @param[out] id_p The address of the returned process id.
+ *
+ * @return PTL_OK Indicates success.
+ * @return PTL_ARG_INVALID Indicates that an invalid argument was passed.
+ * @return PTL_NO_INIT Indicates that the portals API has not been successfully initialized.
+ */
+int PtlGetId(ptl_handle_ni_t ni_handle, ptl_process_t *id_p)
 {
 	int err;
 	ni_t *ni;
 
+#ifndef NO_ARG_VALIDATION
 	err = gbl_get();
-	if (err) {
-		WARN();
-		return err;
-	}
+	if (err)
+		goto err0;
 
 	err = to_ni(ni_handle, &ni);
-	if (unlikely(err)) {
-		WARN();
+	if (err)
 		goto err1;
-	}
 
 	if (!ni) {
-		WARN();
 		err = PTL_ARG_INVALID;
 		goto err1;
 	}
+#else
+	ni = fast_to_obj(ni_handle);
+#endif
 
-	*id = ni->id;
+	*id_p = ni->id;
 
-	if (debug) {
-		printf("PtlGetId returned id = %x, %x\n", ni->id.phys.nid, ni->id.phys.pid);
-	}
-
+	err = PTL_OK;
 	ni_put(ni);
-	gbl_put();
-	return PTL_OK;
-
+#ifndef NO_ARG_VALIDATION
 err1:
 	gbl_put();
+err0:
+#endif
 	return err;
 }
