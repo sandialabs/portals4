470b5db828ccd4f9224c90f4de2466920bcfc145
bwbarrett@gmail.com <bwbarrett@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-11-08 15:29:49 +0000

    add missing files from last night's commit

---
+++ trunk/src/mc/lib/include/ptl_internal_global.h
@@ -0,0 +1,97 @@
+#ifndef PTL_INTERNAL_GLOBAL_H
+#define PTL_INTERNAL_GLOBAL_H
+
+#include <assert.h>
+
+#include "shared/ptl_connection_manager.h"
+#include "shared/ptl_command_queue.h"
+#include "shared/ptl_command_queue_entry.h"
+
+struct ptl_global_t {
+    int32_t         init_count;
+    int             my_ppe_rank;
+    void*           sharedBase;
+    ptl_ni_limits_t limits;
+    ptl_md_t*       mdBase;
+    int             mdFreeHint;
+    ptl_cm_client_handle_t cm_h;
+    ptl_cq_handle_t cq_h;
+};
+typedef struct ptl_global_t ptl_global_t;
+extern ptl_global_t ptl_global;
+
+
+static inline int
+PtlInternalLibraryInitialized(void)
+{
+    return (ptl_global.init_count > 0) ? PTL_OK : PTL_FAIL;
+}
+
+static inline ptl_cq_handle_t
+get_cq_handle(void)
+{
+    return ptl_global.cq_h;
+}
+
+static inline int
+get_cq_peer(void)
+{
+    return 0;
+}
+
+static inline int
+find_md_index(int ni)
+{
+    int index = -1;
+    int cnt = ptl_global.limits.max_mds;
+    while ( cnt-- ) { 
+        // use options as avail flag 
+        if ( ! ptl_global.mdBase[ ptl_global.mdFreeHint  ].options ) {
+            index = ptl_global.mdFreeHint;
+            ++ptl_global.mdFreeHint;
+
+            ptl_global.mdFreeHint %= ptl_global.limits.max_mds; 
+            break;
+        }
+    } 
+    assert( index != -1 );
+    return index;
+}
+
+static inline int
+find_ct_index(int ni)
+{
+    return 0;
+}
+
+static inline int
+find_eq_index(int ni)
+{
+    return 0;
+}
+
+static inline int
+find_le_index(int ni)
+{
+    return 0;
+}
+
+static inline int
+find_me_index(int ni)
+{
+    return 0;
+}
+
+static inline int
+find_pt_index(int ni)
+{
+    return 0;
+}
+
+static inline int
+get_my_id(void)
+{
+    return ptl_global.my_ppe_rank;
+}
+
+#endif
---
+++ trunk/src/mc/lib/include/ptl_internal_startup.h
@@ -0,0 +1,14 @@
+#ifndef PTL_INTERNAL_STARTUP_H
+#define PTL_INTERNAL_STARTUP_H
+
+int ptl_ppe_global_init(void);
+
+int ptl_ppe_global_setup(void);
+
+int ptl_ppe_global_fini(void);
+
+int ptl_ppe_connect(void);
+
+int ptl_ppe_disconnect(void);
+
+#endif
