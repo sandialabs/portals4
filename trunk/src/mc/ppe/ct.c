4a87412417ff97ac02542bd5852583386d4432a3
mjleven@sandia.gov <mjleven@sandia.gov@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-11-16 22:36:22 +0000

    fill in some of the CT stuff

--- trunk/src/mc/ppe/ct.c
+++ trunk/src/mc/ppe/ct.c
@@ -6,27 +6,49 @@
 int
 ct_alloc_impl( ptl_ppe_t *ctx, ptl_cqe_ctalloc_t *cmd )
 {
-    PPE_DBG("\n");
+    PPE_DBG("selector%d code=%d ni=%d\n", cmd->ct_handle.s.selector, 
+                    cmd->ct_handle.s.code, cmd->ct_handle.s.ni );
+
     return 0;
 }
 
 int
 ct_free_impl( ptl_ppe_t *ctx, ptl_cqe_ctfree_t *cmd )
 {
-    PPE_DBG("\n");
+    PPE_DBG("selector%d code=%d ni=%d\n", cmd->ct_handle.s.selector, 
+                    cmd->ct_handle.s.code, cmd->ct_handle.s.ni);
+    ptl_ppe_ni_t *n = 
+                &ctx->clients[cmd->base.remote_id].nis[cmd->ct_handle.s.ni];
+    ptl_internal_ct_t *ct =  n->client_ct + cmd->ct_handle.s.code;
+
+    ct->in_use = 0;
     return 0;
 }
 
 int
 ct_set_impl( ptl_ppe_t *ctx, ptl_cqe_ctset_t *cmd )
 {
-    PPE_DBG("\n");
+    PPE_DBG("selector%d code=%d ni=%d\n", cmd->ct_handle.s.selector, 
+                    cmd->ct_handle.s.code, cmd->ct_handle.s.ni );
+
+    ptl_ppe_ni_t *n = 
+                &ctx->clients[cmd->base.remote_id].nis[cmd->ct_handle.s.ni];
+    ptl_internal_ct_t *ct =  n->client_ct + cmd->ct_handle.s.code;
+    
+    ct->ct_event = cmd->new_ct;
     return 0;
 }
 
 int
 ct_inc_impl( ptl_ppe_t *ctx, ptl_cqe_ctinc_t *cmd )
 {
-    PPE_DBG("\n");
+    PPE_DBG("selector%d code=%d ni=%d\n", cmd->ct_handle.s.selector, 
+                    cmd->ct_handle.s.code, cmd->ct_handle.s.ni );
+
+    ptl_ppe_ni_t *n = 
+                &ctx->clients[cmd->base.remote_id].nis[cmd->ct_handle.s.ni];
+    ptl_internal_ct_t *ct =  n->client_ct + cmd->ct_handle.s.code;
+
+    ct->ct_event.success += cmd->increment.success;
     return 0;
 }
