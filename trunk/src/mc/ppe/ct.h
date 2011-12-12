88e87826ee7c7dc3508b44aadddc483623a2d19a
mjleven@sandia.gov <mjleven@sandia.gov@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2011-12-12 17:35:10 +0000

    check to see if the CT is in use before inc/set

--- trunk/src/mc/ppe/ct.h
+++ trunk/src/mc/ppe/ct.h
@@ -11,8 +11,12 @@ static inline void
 ct_inc( ptl_ppe_ni_t *ppe_ni, ptl_ppe_handle_t ct_handle, ptl_size_t increment)
 {
     PPE_DBG( "ct_index=%d value=%lu\n", ct_handle, increment );
-    ppe_ni->client_ct[ ct_handle ].ct_event.success += increment;
-    ct_pull_triggers( ppe_ni, ct_handle );
+    if ( ppe_ni->ppe_ct[ ct_handle ].in_use ) {
+        ppe_ni->client_ct[ ct_handle ].ct_event.success += increment;
+        ct_pull_triggers( ppe_ni, ct_handle );
+    } else {
+        fprintf(stderr,"%s():%d, unused ct %d\n",__func__,__LINE__,ct_handle);
+    }
 }
 
 
@@ -30,9 +34,12 @@ ct_set( ptl_ppe_ni_t *ppe_ni, ptl_ppe_handle_t ct_handle,
 {
     PPE_DBG("ct_index=%d value=%lu\n", ct_handle, new_event.success );
 
-    ppe_ni->client_ct[ ct_handle ].ct_event = new_event; 
-
-    ct_pull_triggers( ppe_ni, ct_handle );
+    if ( ppe_ni->ppe_ct[ ct_handle ].in_use ) {
+        ppe_ni->client_ct[ ct_handle ].ct_event = new_event; 
+        ct_pull_triggers( ppe_ni, ct_handle );
+    } else {
+        fprintf(stderr,"%s():%d, unused ct %d\n",__func__,__LINE__,ct_handle);
+    }
     return 0;
 }
 
