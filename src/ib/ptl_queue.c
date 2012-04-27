892537d85611035f733f8ac85b828f13235d59b9
fzsfw1 <fzsfw1@gmail.com>
2012-04-27 16:27:21 +0000

    Enqueue objects in shared memory, not buf_t. This open the door to queueing different kind of objects.

--- src/ib/ptl_queue.c
+++ src/ib/ptl_queue.c
@@ -6,7 +6,7 @@
 
 /** Convert an offset in shared memory buffer to buf */
 #define OFF2PTR(commpad, off) (((off) == 0) ? NULL : \
-       (buf_t *)((unsigned char *)(commpad) + (off)))
+       (obj_t *)((unsigned char *)(commpad) + (off)))
 
 /** convert a buf pointer to an offset in shared memory region */
 #define PTR2OFF(commpad, ptr) ((void *)(ptr) - (commpad))
@@ -15,14 +15,16 @@
  * @brief enqueue a buf on a queue.
  *
  * @param[in] queue the queue.
- * @param[in] buf the buf.
+ * @param[in] obj the object to enqueue.
  */
-void enqueue(const void *comm_pad, queue_t *restrict queue, buf_t *buf)
+void enqueue(const void *comm_pad, queue_t *restrict queue, obj_t *obj)
 {
 	unsigned long off;
 	unsigned long off_prev;
 
-	off = PTR2OFF(comm_pad, buf);
+	obj->next = NULL;
+
+	off = PTR2OFF(comm_pad, obj);
 	off_prev = (uintptr_t)atomic_swap_ptr(
 				(void **)(uintptr_t)&(queue->tail),
 				(void *)(uintptr_t)off);
@@ -30,7 +32,7 @@ void enqueue(const void *comm_pad, queue_t *restrict queue, buf_t *buf)
 	if (off_prev == 0)
 		queue->head = off;
 	else
-		OFF2PTR(comm_pad, off_prev)->obj.next = (void *)off;
+		OFF2PTR(comm_pad, off_prev)->next = (void *)off;
 }
 
 /**
@@ -38,11 +40,11 @@ void enqueue(const void *comm_pad, queue_t *restrict queue, buf_t *buf)
  *
  * @param[in] queue the queue.
  *
- * @return a buf.
+ * @return an object.
  */
-buf_t *dequeue(const void *comm_pad, queue_t *queue)
+obj_t *dequeue(const void *comm_pad, queue_t *queue)
 {
-	buf_t *buf;
+	obj_t *obj;
 
 	if (!queue->shadow_head) {
 		if (!queue->head)
@@ -51,30 +53,30 @@ buf_t *dequeue(const void *comm_pad, queue_t *queue)
 		queue->head	= 0;
 	}
 
-	buf = OFF2PTR(comm_pad, queue->shadow_head);
+	obj = OFF2PTR(comm_pad, queue->shadow_head);
 
-	if (buf) {
-		if (buf->obj.next) {
-			queue->shadow_head = (uintptr_t)buf->obj.next;
-			buf->obj.next = NULL;
+	if (obj) {
+		if (obj->next) {
+			queue->shadow_head = (uintptr_t)obj->next;
+			obj->next = NULL;
 		} else {
 			unsigned long old;
 
 			queue->shadow_head = 0;
 			old = (uintptr_t)__sync_val_compare_and_swap(
-				&(queue->tail), PTR2OFF(comm_pad, buf), 0);
+				&(queue->tail), PTR2OFF(comm_pad, obj), 0);
 
-			if (old != PTR2OFF(comm_pad, buf)) {
-				while (buf->obj.next == NULL)
+			if (old != PTR2OFF(comm_pad, obj)) {
+				while (obj->next == NULL)
 					SPINLOCK_BODY();
 
-				queue->shadow_head = (uintptr_t)buf->obj.next;
-				buf->obj.next = NULL;
+				queue->shadow_head = (uintptr_t)obj->next;
+				obj->next = NULL;
 			}
 		}
 	}
 
-	return buf;
+	return obj;
 }
 
 /**
