0a4a7baf98ebadfc31d10da45f71aee2283d2080
fzsfw1@gmail.com <fzsfw1@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2012-07-02 19:15:51 +0000

    Some PPE cleanup. Use a RB tree to store sets of logical NIs.

--- trunk/src/ib/p4ppe.h
+++ trunk/src/ib/p4ppe.h
@@ -54,4 +54,65 @@ struct client
 
 extern struct transport transport_mem;
 
+/* A set of logical NIs. These NIs come from the same application;
+   ie. all applications using that PPE will have a different set of
+   logical NIs (if any). */
+struct logical_ni_set {
+	RB_ENTRY(logical_ni_set) entry;
+	ni_t **ni;
+	uint32_t hash;
+	int members;				/* number of rank in the set */
+};
+
+struct prog_thread {
+	pthread_t thread;
+
+	/* When to stop the progress thread. */
+	int stop;
+
+	/* Points to own queue in comm pad. */
+	queue_t *queue;
+	
+	/* Internal queue. Used for communication regarding transfer
+	 * between clients. */
+	queue_t internal_queue;
+};
+
+struct ppe {
+	/* Communication pad. */
+	struct ppe_comm_pad *comm_pad;
+	struct xpmem_map comm_pad_mapping;
+
+	/* The progress threads. */
+	int num_prog_threads;		/* in prog_thread[] */
+	int current_prog_thread;
+	struct prog_thread prog_thread[MAX_PROGRESS_THREADS];
+
+	/* The event loop thread. */
+	pthread_t		event_thread;
+	int			event_thread_run;
+
+	/* Pool/list of ppebufs, used by client to talk to PPE. */
+	struct {
+		void *slab;
+		int num;				/* total number of ppebufs */
+	} ppebuf;
+
+	/* Tree for physical NIs, indexed on PID. */
+	RB_HEAD(phys_ni_root, ni) physni_tree;
+
+	/* Tree for sets of logical NIs, indexed on hash. */
+	RB_HEAD(logical_ni_set_root, logical_ni_set) set_tree;
+
+	/* Watcher for incoming connection from clients. */
+	ev_io client_watcher;
+	int client_fd;
+
+	/* Linked list of active NIs. */
+	struct list_head ni_list;
+
+	/* List of existing clients. May replace with a tree someday. */
+	struct list_head clients;
+};
+
 #endif /* PTL_GBL_H */
