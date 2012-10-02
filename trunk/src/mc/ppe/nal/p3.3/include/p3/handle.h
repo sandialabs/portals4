aa0d0d3d13a9f92222944181d9a20f3345c1c174
bwbarrett@gmail.com <bwbarrett@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2012-10-02 14:28:55 +0000

    Remove the mc (multi-core) implementation, as that's now part of the ib implementation

--- trunk/src/mc/ppe/nal/p3.3/include/p3/handle.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef _PTL3_P3_HANDLE_H_
-#define _PTL3_P3_HANDLE_H_
-
-#define PTL_HNDL_IF_BITS     3
-#define PTL_MAX_INTERFACES   (1U<<PTL_HNDL_IF_BITS)
-
-#endif
--- trunk/src/mc/ppe/nal/p3.3/include/p3/lock.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PTL3_P3_LOCK_H_
-#define _PTL3_P3_LOCK_H_
-
-#define p3_unlock(x)
-#define p3_lock(x)
-#define p3lock(x)
-#define p3lock_init(x)
-
-#endif
