caf1734d0fb634daf2f638550c276d3d6722465a
bwbarrett@gmail.com <bwbarrett@gmail.com@4e5cb362-e38f-11de-a053-3b9a60e514ca>
2013-06-27 21:45:36 +0000

    Fix the test to actually work:   * Disable comm events for the overflow list so that they don't muddy the waters (no more erroneous reports of an unexpected PUT event).   * Make sure to send all the messages BEFORE searching (oops)   * Use PtlEQPoll instead of PtlEQWait to detect events not showing up.

--- trunk/test/basic/test_persistent_search.c
+++ trunk/test/basic/test_persistent_search.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
+#include <unistd.h>
 
 #include "testing.h"
 
@@ -13,7 +14,8 @@
 # define ENTRY_T  ptl_me_t
 # define HANDLE_T ptl_handle_me_t
 # define NI_TYPE  PTL_NI_MATCHING
-# define OPTIONS  (PTL_ME_OP_PUT | PTL_ME_EVENT_LINK_DISABLE)
+# define AOPTIONS  (PTL_ME_OP_PUT | PTL_ME_EVENT_LINK_DISABLE | PTL_ME_EVENT_COMM_DISABLE)
+# define SOPTIONS  (PTL_ME_OP_PUT | PTL_ME_EVENT_LINK_DISABLE)
 # define APPEND   PtlMEAppend
 # define UNLINK   PtlMEUnlink
 # define SEARCH   PtlMESearch
@@ -21,7 +23,8 @@
 # define ENTRY_T  ptl_le_t
 # define HANDLE_T ptl_handle_le_t
 # define NI_TYPE  PTL_NI_NO_MATCHING
-# define OPTIONS  (PTL_LE_OP_PUT | PTL_LE_EVENT_LINK_DISABLE)
+# define AOPTIONS  (PTL_LE_OP_PUT | PTL_LE_EVENT_LINK_DISABLE | PTL_LE_EVENT_COMM_DISABLE)
+# define SOPTIONS  (PTL_LE_OP_PUT | PTL_LE_EVENT_LINK_DISABLE)
 # define APPEND   PtlLEAppend
 # define UNLINK   PtlLEUnlink
 # define SEARCH   PtlLESearch
@@ -70,7 +73,7 @@ int main(int   argc,
         value_e.match_bits    = 1;
         value_e.ignore_bits   = 0;
 #endif
-        value_e.options = OPTIONS;
+        value_e.options = AOPTIONS;
         CHECK_RETURNVAL(PtlCTAlloc(ni_h, &value_e.ct_handle));
         CHECK_RETURNVAL(APPEND(ni_h, 0, &value_e, PTL_OVERFLOW_LIST, NULL,
                                &value_e_handle));
@@ -90,76 +93,8 @@ int main(int   argc,
 
     libtest_barrier();
 
-    /* 0 writes to 1 */
-    if (1 == rank) {
-        int count = 0;
-        int ret;
-        ptl_event_t ev;
-
-        value_e.start  = &value;
-        value_e.length = sizeof(uint64_t);
-        value_e.uid    = PTL_UID_ANY;
-#if MATCHING == 1
-        value_e.match_id.rank = PTL_RANK_ANY;
-        value_e.match_bits    = 1;
-        value_e.ignore_bits   = 0;
-#endif
-        value_e.options = OPTIONS;
-        CHECK_RETURNVAL(SEARCH(ni_h, 0, &value_e, PTL_SEARCH_ONLY, NULL));
-
-        while (count < 5) {
-            ret = PtlEQWait(eq_h, &ev);
-            if (PTL_OK == ret) {
-                if (PTL_EVENT_SEARCH != ev.type) {
-                    printf("1: SEARCH ONLY: Got event of type %d, expecting %d\n", ev.type, PTL_EVENT_SEARCH);
-                    return 1;
-                }
-                count++;
-            } else {
-                fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
-                return 1;
-            }
-        }
-
-        ret = PtlEQGet(eq_h, &ev);
-        if (PTL_OK == ret) {
-            printf("1: SEARCH ONLY: got unexpected ok of type %d after clearing queue\n", ev.type);
-            return 1;
-        } else if (PTL_EQ_EMPTY) {
-            ;
-        } else {
-            fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
-            return 1;
-        }
-
-        CHECK_RETURNVAL(SEARCH(ni_h, 0, &value_e, PTL_SEARCH_DELETE, NULL));
-
-        while (count < 5) {
-            ret = PtlEQWait(eq_h, &ev);
-            if (PTL_OK == ret) {
-                if (PTL_EVENT_PUT_OVERFLOW != ev.type) {
-                    printf("1: SEARCH DELETE: Got event of type %d, expecting %d\n", ev.type, PTL_EVENT_PUT_OVERFLOW);
-                    return 1;
-                }
-                count++;
-            } else {
-                fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
-                return 1;
-            }
-        }
-
-        ret = PtlEQGet(eq_h, &ev);
-        if (PTL_OK == ret) {
-            printf("1: SEARCH DELETE: got unexpected ok of type %d after clearing queue\n", ev.type);
-            return 1;
-        } else if (PTL_EQ_EMPTY) {
-            ;
-        } else {
-            fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
-            return 1;
-        }
-        
-    } else if (0 == rank) {
+    /* 0 writes unexpecteds to 1 */
+    if (0 == rank) {
         /* write to rank 1 */
         ptl_process_t peer;
 	peer.rank = 1;
@@ -176,6 +111,86 @@ int main(int   argc,
         CHECK_RETURNVAL(PtlCTWait(write_md.ct_handle, 5, &ctc));
         assert(ctc.failure == 0);
     }
+
+
+    libtest_barrier();
+
+    /* 1 does searching */
+    if (1 == rank) {
+        int count = 0;
+        int ret;
+        ptl_event_t ev;
+        unsigned int which;
+
+        value_e.start  = &value;
+        value_e.length = sizeof(uint64_t);
+        value_e.uid    = PTL_UID_ANY;
+#if MATCHING == 1
+        value_e.match_id.rank = PTL_RANK_ANY;
+        value_e.match_bits    = 1;
+        value_e.ignore_bits   = 0;
+#endif
+        value_e.options = SOPTIONS;
+
+        CHECK_RETURNVAL(SEARCH(ni_h, 0, &value_e, PTL_SEARCH_ONLY, NULL));
+        while (count < 5) {
+            ret = PtlEQPoll(&eq_h, 1, 1000, &ev, &which);
+            if (PTL_OK == ret) {
+                if (PTL_EVENT_SEARCH != ev.type) {
+                    printf("1: SEARCH ONLY: Got event of type %d, expecting %d\n", ev.type, PTL_EVENT_SEARCH);
+                    return 1;
+                }
+                count++;
+            } else if (PTL_EQ_EMPTY) {
+                fprintf(stderr, "1: SEARCH ONLY: unexpectedly found empty queue\n");
+                return 1;
+            } else {
+                fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
+                return 1;
+            }
+            sleep(1);
+        }
+
+        ret = PtlEQGet(eq_h, &ev);
+        if (PTL_OK == ret) {
+            printf("1: SEARCH ONLY: got unexpected ok of type %d after clearing queue\n", ev.type);
+            return 1;
+        } else if (PTL_EQ_EMPTY) {
+            ;
+        } else {
+            fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
+            return 1;
+        }
+
+        CHECK_RETURNVAL(SEARCH(ni_h, 0, &value_e, PTL_SEARCH_DELETE, NULL));
+        while (count < 5) {
+            ret = PtlEQPoll(&eq_h, 1, 1000, &ev, &which);
+            if (PTL_OK == ret) {
+                if (PTL_EVENT_PUT_OVERFLOW != ev.type) {
+                    printf("1: SEARCH DELETE: Got event of type %d, expecting %d\n", ev.type, PTL_EVENT_PUT_OVERFLOW);
+                    return 1;
+                }
+                count++;
+            } else if (PTL_EQ_EMPTY) {
+                fprintf(stderr, "1: SEARCH DELETE: unexpectedly found empty queue\n");
+                return 1;
+            } else {
+                fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
+                return 1;
+            }
+        }
+
+        ret = PtlEQGet(eq_h, &ev);
+        if (PTL_OK == ret) {
+            printf("1: SEARCH DELETE: got unexpected ok of type %d after clearing queue\n", ev.type);
+            return 1;
+        } else if (PTL_EQ_EMPTY) {
+            ;
+        } else {
+            fprintf(stderr, "1: Unexpected return code from EQWait: %d\n", ret);
+            return 1;
+        }
+    }        
 
     libtest_barrier();
 
