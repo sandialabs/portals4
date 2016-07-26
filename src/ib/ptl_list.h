d58678c86122e0a2e02178a90cbf1d642d7c75e0
James Dinan <james.dinan@intel.com>
2016-07-26 09:13:16 -0400

    Fix type system issues with zero MRs
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- src/ib/ptl_list.h
+++ src/ib/ptl_list.h
@@ -8,9 +8,11 @@
 /*
  * list_head ops fragment, adapted from linux/list.h
  */
+#ifndef container_of
 #define container_of(ptr, type, member) ({ \
 	const typeof( ((type *)0)->member ) *__mptr = (ptr); \
 	(type *)( (char *)__mptr - offsetof(type,member) );})
+#endif
 
 struct list_head {
     struct list_head *next;
