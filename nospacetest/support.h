30d0cf1d7a2c1764391693d0c8f29592e8420ffd
Donald Kruse <dkruse@sandia.gov>
2023-09-27 12:07:11 -0600

    Set up initial dir for testing ptl no space

---
+++ nospacetest/support.h
@@ -0,0 +1,170 @@
+/* -*- C -*-
+ *
+ * Copyright 2006 Sandia Corporation. Under the terms of Contract
+ * DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government
+ * retains certain rights in this software.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _LIBP4SUPPORT_H
+#define _LIBP4SUPPORT_H
+
+/*
+ * libtest_init(void)
+ * 
+ * Returns:
+ *  0  - success
+ *  1  - failure
+ *
+ * Notes:
+ *  Must be called before any other libtest function.
+ */
+int libtest_init(void);
+
+
+/*
+ * libtest_fini(void)
+ * 
+ * Returns:
+ *  0  - success
+ *  1  - failure
+ *
+ * Notes:
+ *  No libtest function may be called after calling fini.
+ */
+int libtest_fini(void);
+
+
+/*
+ * libtest_get_mapping(ptl_handle_ni_t ni_h)
+ * 
+ * Returns:
+ *  non-NULL - physical address for every rank in job
+ *  NULL     - Error
+ *
+ * Notes:
+ *  Mapping will be a static buffer; should not be freed by
+ *  the caller.
+ *
+ *  This call is collective and must be called in the same order on
+ *  all processes.
+ */
+ptl_process_t* libtest_get_mapping(ptl_handle_ni_t ni_h);
+
+
+/*
+ * libtest_get_rank(void)
+ * 
+ * Returns:
+ *  -1           - Error
+ *  Non-negative - Rank in job
+ *
+ * Notes:
+ */
+int libtest_get_rank(void);
+
+
+/*
+ * libtest_get_size(void)
+ * 
+ * Returns:
+ *  -1           - Error
+ *  Non-negative - Job size
+ *
+ * Notes:
+ */
+int libtest_get_size(void);
+
+
+/*
+ * libtest_barrier(void)
+ * 
+ * Returns:
+ *
+ * Notes:
+ */
+void libtest_barrier(void);
+
+
+/*
+** Some convenience functions
+*/
+#define LIBTEST_CHECK(rc, fun)                                         \
+    if (rc != PTL_OK && rc != PTL_IGNORED)   {							\
+	fprintf(stderr, "%s() failed (%s)\n", fun, libtest_StrPtlError(rc)); \
+	exit(1);                                                       \
+    }
+
+/* Use this libtest_Put() if no ACKs, user data, etc. */
+#define libtest_Put(handle, size, dest, index)                          \
+    PtlPut(handle, 0, size, PTL_NO_ACK_REQ, dest, index, 0, 0, NULL, 0)
+
+/* Same as above, but with local and remote offsets */
+#define libtest_Put_offset(handle, local_offset, size, dest, index, match, remote_offset) \
+    PtlPut(handle, local_offset, size, PTL_NO_ACK_REQ, dest, index, match, remote_offset, NULL, 0)
+
+/* Use these to setup MDs and LEs, with and without an attached counter */
+ptl_handle_md_t libtest_CreateMD(ptl_handle_ni_t ni, 
+                                 void *start, 
+                                 ptl_size_t length);
+
+void libtest_CreateMDCT(ptl_handle_ni_t ni, 
+                        void *start,
+                        ptl_size_t length,
+                        ptl_handle_md_t *mh,
+                        ptl_handle_ct_t *ch);
+
+void libtest_CreateLECT(ptl_handle_ni_t ni, 
+                        ptl_pt_index_t index,
+                        void *start,
+                        ptl_size_t length,
+                        ptl_handle_le_t *lh,
+                        ptl_handle_ct_t *ch);
+
+int libtest_CreateMEPersistent(ptl_handle_ni_t ni,
+                               ptl_pt_index_t index,
+                               void *start,
+                               ptl_size_t length,
+                               ptl_size_t count, 
+                               ptl_handle_me_t *mh);
+
+int libtest_CreateMEUseOnce(ptl_handle_ni_t ni,
+                            ptl_pt_index_t index,
+                            void *start,
+                            ptl_size_t length,
+                            ptl_size_t count,
+                            ptl_handle_me_t *mh);
+
+void libtest_FreeME(ptl_size_t count,
+                    ptl_handle_me_t *mh);
+
+char * libtest_StrPtlError(int error_code);
+
+/* Use this to get a Portal table entry */
+ptl_pt_index_t libtest_PTAlloc(ptl_handle_ni_t ni,
+                               ptl_pt_index_t request,
+                               ptl_handle_eq_t eq);
+
+void libtest_BarrierInit(ptl_handle_ni_t ni,
+                         int rank,
+                         int nproc);
+void libtest_Barrier(void);
+
+void libtest_AllreduceDouble_init(ptl_handle_ni_t ni);
+double libtest_AllreduceDouble(double value, ptl_op_t op);
+
+#endif /* _LIBP4SUPPORT_H */
---
+++ nospacetest/test_triggered_ctset.c
@@ -0,0 +1,85 @@
+#include <portals4.h>
+#include <support.h>
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sched.h>
+
+#include "testing.h"
+
+int main(int   argc,
+         char *argv[])
+{
+    ptl_handle_ni_t ni_logical;
+    ptl_process_t   myself;
+    int             num_procs;
+    ptl_handle_ct_t trigger, target;
+
+    CHECK_RETURNVAL(PtlInit());
+
+    CHECK_RETURNVAL(libtest_init());
+
+    num_procs = libtest_get_size();
+
+    CHECK_RETURNVAL(PtlNIInit(PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_LOGICAL,
+                              PTL_PID_ANY, NULL, NULL, &ni_logical));
+
+    CHECK_RETURNVAL(PtlSetMap(ni_logical, num_procs,
+                              libtest_get_mapping(ni_logical)));
+
+    CHECK_RETURNVAL(PtlGetId(ni_logical, &myself));
+    CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &trigger));
+    CHECK_RETURNVAL(PtlCTAlloc(ni_logical, &target));
+    /* Now do a barrier (on ni_physical) to make sure that everyone has their
+     * logical interface set up */
+    libtest_barrier();
+
+#ifdef ORDERED
+    CHECK_RETURNVAL(PtlTriggeredCTSet(target, (ptl_ct_event_t) { 1, 0 }, trigger, 1));
+    CHECK_RETURNVAL(PtlTriggeredCTSet(target, (ptl_ct_event_t) { 7, 0 }, trigger, 2));
+#else
+    CHECK_RETURNVAL(PtlTriggeredCTSet(target, (ptl_ct_event_t) { 7, 0 }, trigger, 2));
+    CHECK_RETURNVAL(PtlTriggeredCTSet(target, (ptl_ct_event_t) { 1, 0 }, trigger, 1));
+#endif
+
+    /* check the target and trigger, make sure they're both zero */
+    {
+        ptl_ct_event_t test;
+        CHECK_RETURNVAL(PtlCTGet(target, &test));
+        assert(test.success == 0);
+        assert(test.failure == 0);
+        CHECK_RETURNVAL(PtlCTGet(trigger, &test));
+        assert(test.success == 0);
+        assert(test.failure == 0);
+    }
+    /* Increment the trigger */
+    CHECK_RETURNVAL(PtlCTInc(trigger, (ptl_ct_event_t) { 1, 0 }));
+    /* Check the target */
+    {
+        ptl_ct_event_t test;
+        CHECK_RETURNVAL(PtlCTWait(target, 1, &test));
+        assert(test.success == 1);
+        assert(test.failure == 0);
+    }
+    /* Increment the trigger again */
+    CHECK_RETURNVAL(PtlCTInc(trigger, (ptl_ct_event_t) { 1, 0 }));
+    {
+        ptl_ct_event_t test;
+        CHECK_RETURNVAL(PtlCTWait(target, 7, &test));
+        assert(test.success == 7);
+        assert(test.failure == 0);
+    }
+
+    /* cleanup */
+    CHECK_RETURNVAL(PtlCTFree(trigger));
+    CHECK_RETURNVAL(PtlCTFree(target));
+    CHECK_RETURNVAL(PtlNIFini(ni_logical));
+    CHECK_RETURNVAL(libtest_fini());
+    PtlFini();
+
+    return 0;
+}
+
+/* vim:set expandtab: */
